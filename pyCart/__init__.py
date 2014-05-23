2b4efcb8b44363ab311d3b585853ae8705cbb135
Derek Jordan Dalle <ddalle@linux98.nas.nasa.gov>
2014-05-23 08:32:48 -0700

    Initial commit

---
+++ pyCart/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyCart
+======
+
+A Python interface for Cart3D, a Cartesian-grid, cut-cell CFD tool.
+"""
+
+# Set version number.  Don't forget to update it.
+__version__ = 0.1
+
+# Import parts of the module...
+
