24c2101fbf5f953b87a29a95888e4fafe225b28e
Derek Dalle <derek.dalle@gmail.com>
2014-09-10 22:43:31 -0700

    Added separate module for making commands and running them

--- pyCart/bin.py
+++ pyCart/bin.py
@@ -8,6 +8,9 @@ Cart3D binary interface module: :mod:`pyCart.bin`
 import os
 import subprocess as sp
 
+# Command option processing
+import cmd
+
 # Function to call commands with a different STDOUT
 def callf(cmd, f=None, shell=None):
     """Call a command with alternate STDOUT by filename
@@ -63,196 +66,90 @@ def _assertfile(fname):
     # Check for the file.
     if not os.path.isfile(fname):
         raise IOError("No input file '%s' found." % fname)
+        
+# Function to automate minor changes to docstrings to make them pyCart.Cart3d
+def _upgradeDocString(doccmd):
+    """Upgrade docstrings from the :mod:`pyCart.bin` class
+    
+    :Call:
+        >>> docbin = _upgradDocString(doccmd)
+    :Inputs:
+        *doccmd*: :class:`str`
+            Docstring from method from :mod:`pyCart.cmd`
+    :Outputs:
+        *docbin*: :class:`str`
+            Docstring for :mod:`pyCart.bin`
+    :Versions:
+        * 2014.09.10 ``@ddalle``: First version
+    """
+    # Replace module reference.
+    txt = doccmd.replace(".cmd.", ".bin.")
+    # Delete output in the commands.
+    txt = txt.replace("cmd = ", "")
+    # Initialize output.
+    docbin = ""
+    kflag = True
+    # Loop through lines.
+    for line in txt.split("\n"):
+        # Check the line.
+        if line.lstrip().startswith(":Output"):
+            kflag = False
+        elif line.lstrip().startswith(":Versions:"):
+            kflag = True
+        # Keep the line if the flag is set.
+        if kflag: docbin += (line + "\n")
+    # Output
+    return docbin
 
 # Function to call cubes.
-def cubes(cart3d=None, maxR=10, reorder=True, pre='preSpec.c3d.cntl',
-    cubes_a=None, cubes_b=None):
-    """
-    Interface to Cart3D script `cubes`
-    
-    :Call:
-        >>> pyCart.bin.cubes(cart3d)
-        >>> pyCart.bin.cubes(maxR=10, reorder=True, pre='preSpec.c3d.cntl')
-    :Inputs:
-        *cart3d*: :class:`pyCart.cart3d.Cart3d`
-            Global pyCart settings instance
-        *maxR*: :class:`int`
-            Number of refinements to make
-        *reorder*: :class:`bool`
-            Whether or not to reorder mesh
-        *pre*: :class:`str`
-            Name of prespecified bounding box file (or ``None``)
-        *cubes_a*: :class:`int`
-            `cubes` "a" parameter
-        *cubes_b*: :class:`int`
-            `cubes` "b" parameter
-    :Versions:
-        * 2014.06.30 ``@ddalle``: First version
-        * 2014.09.02 ``@ddalle``: Rewritten with new options paradigm
-    """
-    # Check cart3d input.
-    if cart3d is not None:
-        # Apply values
-        maxR    = cart3d.opts.get_maxR()
-        pre     = cart3d.opts.get_pre()
-        reorder = cart3d.opts.get_reorder()
-        cubes_a = cart3d.opts.get_cubes_a()
-        cubes_b = cart3d.opts.get_cubes_b()
+def cubes(cart3d=None, **kwargs):
     # Required file
     _assertfile('input.c3d')
-    # Initialize command
-    cmd = ['cubes']
-    # Add options.
-    if maxR:    cmd += ['-maxR', str(maxR)]
-    if reorder: cmd += ['-reorder']
-    if pre:     cmd += ['-pre', pre]
-    if cubes_a: cmd += ['-a', str(cubes_a)]
-    if cubes_b: cmd += ['-b', str(cubes_b)]
+    # Get command
+    cmd = cmd.cubes(cart3d=cart3d, **kwargs)
     # Run the command.
     callf(cmd, f='cubes.out')
+# Docstring
+cubes.__doc__ = _upgradeDocString(cmd.cubes.__doc__)
     
 # Function to call mgPrep
-def mgPrep(cart3d=None, mg_fc=3):
-    """
-    Interface to Cart3D script `mgPrep`
-    
-    :Call:
-        >>> pyCart.bin.mgPrep(cart3d)
-        >>> pyCart.bin.mgPrep(mg_fc=3)
-    :Inputs:
-        *cart3d*: :class:`pyCart.cart3d.Cart3d`
-            Global pyCart settings instance
-        *mg_fc*: :class:`int`
-            Number of multigrid levels to prepare
-    :Versions:
-        * 2014.09.02 ``@ddalle``: First version
-    """
-    # Check cart3d input.
-    if cart3d is not None:
-        # Apply values
-        mg_fc = cart3d.opts.get_mg_fc()
+def mgPrep(cart3d=None, **kwargs):
     # Required file
     _assertfile('Mesh.R.c3d')
-    # Initialize command.
-    cmd = ['mgPrep']
-    # Add options.
-    if mg_fc: cmd += ['-n', str(mg_fc)]
+    # Get the command.
+    cmd = cmd.mgPrep(cart3d=cart3d, **kwargs)
     # Run the command.
     callf(cmd, f='mgPrep.out')
+# Docstring
+mgPrep.__doc__ = _upgradeDocString(cmd.mgPrep.__doc__)
     
 # Function to call mgPrep
-def autoInputs(cart3d=None, r=8, ftri='Components.i.tri'):
-    """
-    Interface to Cart3D script `mgPrep`
-    
-    :Call:
-        >>> pyCart.bin.autoInputs(cart3d)
-        >>> pyCart.bin.autoInputs(r=8, ftri='components.i.tri')
-    :Inputs:
-        *cart3d*: :class:`pyCart.cart3d.Cart3d`
-            Global pyCart settings instance
-        *r*: :class:`int`
-            Mesh radius
-        *ftri*: :class:`str`
-            Name of surface triangulation file
-    :Versions:
-        * 2014.09.02 ``@ddalle``: First version
-    """
-    # Check cart3d input.
-    if cart3d is not None:
-        # Apply values
-        mg_fc = cart3d.opts.get_r()
-    # Initialize command.
-    cmd = ['autoInputs']
-    # Add options.
-    if r:    cmd += ['-r', str(mg_fc)]
-    if ftri: cmd += ['-t', ftri]
+def autoInputs(cart3d=None, **kwargs):
+    # Get command.
+    cmd = cmd.autoInputs(cart3d, **kwargs)
     # Run the command.
     callf(cmd, f='autoInputs.out')
+# Docstring
+autoInputs.__doc__ = _upgradeDocString(cmd.autoInputs.__doc__)
     
     
     
 # Function to call flowCart
 def flowCart(cart3d=None, i=0, **kwargs):
-    """Interface to Cart3D binary ``flowCart``
-    
-    :Call:
-        >>> pyCart.bin.flowCart(cart3d, i=0)
-        >>> pyCart.bin.flowCart(i=0, **kwargs)
-    :Inputs:
-        *cart3d*: :class:`pyCart.cart3d.Cart3d`
-            Global pyCart settings instance
-        *i*: :class:`int`
-            Run index (restart if *i* is greater than *0*)
-        *it_fc*: :class:`int`
-            Number of iterations to run ``flowCart``
-        *mg_fc*: :class:`int`
-            Number of multigrid levels to use
-        *cfl*: :class:`float`
-            Nominal CFL number
-        *cflmin*: :class:`float`
-            Minimum CFL number allowed for problem cells or iterations
-        *tm*: :class:`bool`
-            Whether or not to use second-order cut cells
-        *binaryIO*: :class:`bool`
-            Whether or not to use binary format for input/output files
-        *limiter*: :class:`int`
-            Limiter index
-        *y_is_spanwise*: :class:`bool`
-            Whether or not to consider angle of attack in *z* direction
-        *nThreads*: :class:`int`
-            Number of threads to use for ``flowCart`` operation
-    :Versions:
-        * 2014.09.07 ``@ddalle``: First version
-    """
     # Check for cart3d input
     if cart3d is not None:
         # Get values from internal settings.
-        it_fc   = cart3d.opts.get_it_fc(i)
-        limiter = cart3d.opts.get_imiter(i)
-        y_span  = cart3d.opts.get_y_is_spanwise(i)
-        bin_IO  = cart3d.opts.get_binaryIO(i)
-        tecO    = cart3d.opts.get_tecO(i)
-        cfl     = cart3d.opts.get_cfl(i)
-        cflmin  = cart3d.opts.get_cflmin(i)
-        mg_fc   = cart3d.opts.get_mg_fc(i)
-        tm      = cart3d.opts.get_tm(i)
-        nThread = cart3d.opts.get_OMP_NUM_THREADS(i)
+        nProc   = cart3d.opts.get_OMP_NUM_THREADS(i)
     else:
         # Get values from keyword arguments
-        it_fc   = kwargs.get('it_fc', 200)
-        limiter = kwargs.get('limiter', 2)
-        y_span  = kwargs.get('y_is_spanwise', True)
-        binIO   = kwargs.get('binaryIO', False)
-        tecO    = kwargs.get('tecO', False)
-        cfl     = kwargs.get('cfl', 1.1)
-        cflmin  = kwargs.get('cflmin', 0.8)
-        mg_fc   = kwargs.get('mg_fc', 3)
-        tm      = kwargs.get('tm', None)
-        nThread = kwargs.get('nThreads', 4)
+        nProc   = kwargs.get('nProc', 4)
     # Set environment variable.
-    if nThreads:
-        os.environ['OMP_NUM_THREADS'] = str(nThreads)
-    # Initialize command.
-    cmd = ['flowCart', '-his', '-v']
-    # Check for restart
-    if restart: cmd += ['-restart']
-    # Add options
-    if it_fc:   cmd += ['-N', str(it_fc)]
-    if y_span:  cmd += ['-y_is_spanwise']
-    if limiter: cmd += ['-limiter', str(limiter)]
-    if binIO:   cmd += ['-binaryIO']
-    if tecO:    cmd += ['-T']
-    if cfl:     cmd += ['-cfl', str(cfl)]
-    if cflmin:  cmd += ['-cflmin', str(cflmin)]
-    if mg_fc:   cmd += ['-mg', str(mg_fc)]
-    # Process and translate the cut-cell gradient variable.
-    if (tm is not None) and tm:
-        # Second-order cut cells
-        cmd += ['-tm', '1']
-    elif (tm is not None) and (not tm):
-        # First-order cut cells
-        cmd += ['-tm', '0']
+    if nProc:
+        os.environ['OMP_NUM_THREADS'] = str(nProc)
+    # Get command.
+    cmd = cmd.flowCart(cart3d=cart3d, i=i, **kwargs)
     # Run the command
     callf(cmd, f='flowCart.out')
+# Docstring
+flowCart.__doc__ = _upgradeDocString(cmd.flowCart.__doc__)
 
---
+++ pyCart/cmd.py
@@ -0,0 +1,203 @@
+"""
+Create command strings for Cart3D binaries: :mod:`pyCart.cmd`
+=============================================================
+
+"""
+
+
+# Function to call cubes.
+def cubes(cart3d=None, maxR=10, reorder=True, pre='preSpec.c3d.cntl',
+    cubes_a=None, cubes_b=None):
+    """
+    Interface to Cart3D script `cubes`
+    
+    :Call:
+        >>> cmd = pyCart.cmd.cubes(cart3d)
+        >>> cmd = pyCart.cmd.cubes(maxR=10, reorder=True, pre='preSpec.c3d.cntl')
+    :Inputs:
+        *cart3d*: :class:`pyCart.cart3d.Cart3d`
+            Global pyCart settings instance
+        *maxR*: :class:`int`
+            Number of refinements to make
+        *reorder*: :class:`bool`
+            Whether or not to reorder mesh
+        *pre*: :class:`str`
+            Name of prespecified bounding box file (or ``None``)
+        *cubes_a*: :class:`int`
+            `cubes` "a" parameter
+        *cubes_b*: :class:`int`
+            `cubes` "b" parameter
+    :Outputs:
+        *cmd*: :class:`list` (:class:`str`)
+            Command split into a list of strings
+    :Versions:
+        * 2014.06.30 ``@ddalle``: First version
+        * 2014.09.02 ``@ddalle``: Rewritten with new options paradigm
+        * 2014.09.10 ``@ddalle``: Split into cmd and bin
+    """
+    # Check cart3d input.
+    if cart3d is not None:
+        # Apply values
+        maxR    = cart3d.opts.get_maxR()
+        pre     = cart3d.opts.get_pre()
+        reorder = cart3d.opts.get_reorder()
+        cubes_a = cart3d.opts.get_cubes_a()
+        cubes_b = cart3d.opts.get_cubes_b()
+    # Initialize command
+    cmd = ['cubes']
+    # Add options.
+    if maxR:    cmd += ['-maxR', str(maxR)]
+    if reorder: cmd += ['-reorder']
+    if pre:     cmd += ['-pre', pre]
+    if cubes_a: cmd += ['-a', str(cubes_a)]
+    if cubes_b: cmd += ['-b', str(cubes_b)]
+    # Return the command.
+    return cmd
+    
+# Function to call mgPrep
+def mgPrep(cart3d=None, mg_fc=3):
+    """
+    Interface to Cart3D script `mgPrep`
+    
+    :Call:
+        >>> cmd = pyCart.cmd.mgPrep(cart3d)
+        >>> cmd = pyCart.cmd.mgPrep(mg_fc=3)
+    :Inputs:
+        *cart3d*: :class:`pyCart.cart3d.Cart3d`
+            Global pyCart settings instance
+        *mg_fc*: :class:`int`
+            Number of multigrid levels to prepare
+    :Outputs:
+        *cmd*: :class:`list` (:class:`str`)
+            Command split into a list of strings
+    :Versions:
+        * 2014.09.02 ``@ddalle``: First version
+    """
+    # Check cart3d input.
+    if cart3d is not None:
+        # Apply values
+        mg_fc = cart3d.opts.get_mg_fc()
+    # Initialize command.
+    cmd = ['mgPrep']
+    # Add options.
+    if mg_fc: cmd += ['-n', str(mg_fc)]
+    # Return the command
+    return cmd
+    
+# Function to call mgPrep
+def autoInputs(cart3d=None, r=8, ftri='Components.i.tri'):
+    """
+    Interface to Cart3D script `mgPrep`
+    
+    :Call:
+        >>> cmd = pyCart.cmd.autoInputs(cart3d)
+        >>> cmd = pyCart.cmd.autoInputs(r=8, ftri='components.i.tri')
+    :Inputs:
+        *cart3d*: :class:`pyCart.cart3d.Cart3d`
+            Global pyCart settings instance
+        *r*: :class:`int`
+            Mesh radius
+        *ftri*: :class:`str`
+            Name of surface triangulation file
+    :Outputs:
+        *cmd*: :class:`list` (:class:`str`)
+            Command split into a list of strings
+    :Versions:
+        * 2014.09.02 ``@ddalle``: First version
+    """
+    # Check cart3d input.
+    if cart3d is not None:
+        # Apply values
+        mg_fc = cart3d.opts.get_r()
+        ftri  = cart3d.opts.get_TriFile()
+    # Initialize command.
+    cmd = ['autoInputs']
+    # Add options.
+    if r:    cmd += ['-r', str(mg_fc)]
+    if ftri: cmd += ['-t', ftri]
+    # Return the command.
+    return cmd
+
+# Function to call flowCart
+def flowCart(cart3d=None, i=0, **kwargs):
+    """Interface to Cart3D binary ``flowCart``
+    
+    :Call:
+        >>> cmd = pyCart.cmd.flowCart(cart3d, i=0)
+        >>> cmd = pyCart.cmd.flowCart(**kwargs)
+    :Inputs:
+        *cart3d*: :class:`pyCart.cart3d.Cart3d`
+            Global pyCart settings instance
+        *i*: :class:`int`
+            Run index (restart if *i* is greater than *0*)
+        *it_fc*: :class:`int`
+            Number of iterations to run ``flowCart``
+        *mg_fc*: :class:`int`
+            Number of multigrid levels to use
+        *cfl*: :class:`float`
+            Nominal CFL number
+        *cflmin*: :class:`float`
+            Minimum CFL number allowed for problem cells or iterations
+        *tm*: :class:`bool`
+            Whether or not to use second-order cut cells
+        *binaryIO*: :class:`bool`
+            Whether or not to use binary format for input/output files
+        *limiter*: :class:`int`
+            Limiter index
+        *y_is_spanwise*: :class:`bool`
+            Whether or not to consider angle of attack in *z* direction
+        *nProc*: :class:`int`
+            Number of threads to use for ``flowCart`` operation
+    :Outputs:
+        *cmd*: :class:`list` (:class:`str`)
+            Command split into a list of strings
+    :Versions:
+        * 2014.09.07 ``@ddalle``: First version
+    """
+    # Check for cart3d input
+    if cart3d is not None:
+        # Get values from internal settings.
+        it_fc   = cart3d.opts.get_it_fc(i)
+        limiter = cart3d.opts.get_imiter(i)
+        y_span  = cart3d.opts.get_y_is_spanwise(i)
+        bin_IO  = cart3d.opts.get_binaryIO(i)
+        tecO    = cart3d.opts.get_tecO(i)
+        cfl     = cart3d.opts.get_cfl(i)
+        cflmin  = cart3d.opts.get_cflmin(i)
+        mg_fc   = cart3d.opts.get_mg_fc(i)
+        tm      = cart3d.opts.get_tm(i)
+    else:
+        # Get values from keyword arguments
+        it_fc   = kwargs.get('it_fc', 200)
+        limiter = kwargs.get('limiter', 2)
+        y_span  = kwargs.get('y_is_spanwise', True)
+        binIO   = kwargs.get('binaryIO', False)
+        tecO    = kwargs.get('tecO', False)
+        cfl     = kwargs.get('cfl', 1.1)
+        cflmin  = kwargs.get('cflmin', 0.8)
+        mg_fc   = kwargs.get('mg_fc', 3)
+        tm      = kwargs.get('tm', None)
+    # Initialize command.
+    cmd = ['flowCart', '-his', '-v']
+    # Check for restart
+    if (i>0): cmd += ['-restart']
+    # Add options
+    if it_fc:   cmd += ['-N', str(it_fc)]
+    if y_span:  cmd += ['-y_is_spanwise']
+    if limiter: cmd += ['-limiter', str(limiter)]
+    if binIO:   cmd += ['-binaryIO']
+    if tecO:    cmd += ['-T']
+    if cfl:     cmd += ['-cfl', str(cfl)]
+    if cflmin:  cmd += ['-cflmin', str(cflmin)]
+    if mg_fc:   cmd += ['-mg', str(mg_fc)]
+    # Process and translate the cut-cell gradient variable.
+    if (tm is not None) and tm:
+        # Second-order cut cells
+        cmd += ['-tm', '1']
+    elif (tm is not None) and (not tm):
+        # First-order cut cells
+        cmd += ['-tm', '0']
+    # Output
+    return cmd
+
+
