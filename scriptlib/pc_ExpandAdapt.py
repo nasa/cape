49748be9f2f4efd112be6855bf0982b66ee3b0ec
Derek J. Dalle <derek.j.dalle@nasa.gov>
2014-12-30 08:32:31 -0800

    Script to expand tarred adapt folders

---
+++ scriptlib/pc_ExpandAdapt.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+Expand tarred ``adapt??.tar`` archives: :file:`pc_ExpandAdapt.py`
+=================================================================
+
+Expand all :file:`adapt??.tar` files and delete the tar balls.
+
+    * :file:`adapt00.tar` --> ``adapt00/``
+    * :file:`adapt01.tar` --> ``adapt01/``
+    * ``adapt02/`` --> ``adapt02/`
+
+:Call:
+
+    .. code-block:: console
+    
+        $ pc_ExpandAdapt.py [OPTIONS]
+    
+:Options:
+    -h, --help
+        Display this help message and exit
+        
+:Versions:
+    * 2014-12-30 ``@ddalle``: First version
+"""
+
+# Modules
+import pyCart.argread
+import pyCart.manage
+# System interface.
+import sys
+        
+    
+# Check for running as a script.
+if __name__ == "__main__":
+    # Process inputs.
+    a, kw = pyCart.argread.readkeys(sys.argv)
+    # Check for help flag.
+    if kw.get('h') or kw.get('help'):
+        # Display help message and exit.
+        print(__doc__)
+        sys.exit()
+    # Do the plotting.
+    pyCart.manage.ExpandAdapt()
+    
