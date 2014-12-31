3f7186a930465bb5f8ed97cbc99da563d4ca25d8
Derek J. Dalle <derek.j.dalle@nasa.gov>
2014-12-30 18:44:12 -0800

    Debugging of CarpetPlot

--- pyCart/dataBook.py
+++ pyCart/dataBook.py
@@ -470,6 +470,7 @@ class DataBook(dict):
             else:
                 # Use literal string conversion.
                 tags.append('%s=%s' % (k, DBc[k][I[0]]))
+        print("tags: %s" % tags)
                 
         # Check for carpet.
         o_carpet = DBP["Carpet"]
@@ -720,6 +721,7 @@ class DataBook(dict):
             for jj in range(len(I)):
                 # Extract indices
                 j = I[jj]
+                print("  j=%s" % j)
                 # Form the additional label using initial value of *cv*.
                 f_lgnd = s_lgnd % DBc[cv][j[0]]
                 # Call the individual sweep plot function.
