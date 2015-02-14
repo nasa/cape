59ff9313257d146addb9e2875cfb666139c56856
Derek J. Dalle <derek.j.dalle@nasa.gov>
2015-02-14 10:50:16 -0800

    Typo in run_verify.py

--- scriptlib/run_verify.py
+++ scriptlib/run_verify.py
@@ -7,7 +7,7 @@ This script determines the appropriate index to run for an individual case (e.g.
 if a restart is appropriate, etc.), sets that case up, and runs it.
 
 :Versions:
-    * 2014-10-02 ``@ddalle``: First version
+    * 2014-02-14 ``@ddalle``: First version
 """
 
 # Import the module specifically for this task.
@@ -22,7 +22,7 @@ def run_verify():
     :Versions:
         * 2015-02-13 ``@ddalle``: First version
     """
-    pyCart.case.run_flowCart()
+    pyCart.bin.verify()
     
 # Check if run as a script.
 if __name__ == "__main__":
