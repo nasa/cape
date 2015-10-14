79798a174fa4a93d2ccdefe9b7da1ebe2442263b
Derek J. Dalle <derek.j.dalle@nasa.gov>
2015-10-14 12:13:08 -0700

    Readded pyCart.config that just points to cape.config

---
+++ pyCart/config.py
@@ -0,0 +1,10 @@
+"""
+Module to interface with Cart3D configuration files: :mod:`pyCart.Config`
+=========================================================================
+
+This is a module to interact with :file:`Config.xml` files.
+"""
+
+# Import from CAPE
+from cape.config import Config
+
