b284b05e6173586a93d956686b6f9e0b262028ae
Derek J. Dalle <derek.j.dalle@nasa.gov>
2016-04-28 10:46:12 -0700

    Made command-line call generator for dual

--- pyFun/cmd.py
+++ pyFun/cmd.py
@@ -12,8 +12,8 @@ instance.
 
 # File checking.
 import os.path
-# Get command to run tecplot
-from .util import GetTecplotCommand
+# Options for the binaries
+from .options import runControl
 
 
 # Function to create ``nodet`` or ``nodet_mpi`` command
@@ -27,7 +27,7 @@ def nodet(opts=None, i=0, **kw):
         *opts*: :class:`pyFun.options.Options`
             Global pyFun options interface or "RunControl" interface
         *i*: :class:`int`
-            Run sequence index
+            Phase number
         *animation_freq*: :class:`int`
             Output frequency
     :Outputs:
@@ -92,4 +92,97 @@ def nodet(opts=None, i=0, **kw):
             cmdi.append(str(v))
     # Output
     return cmdi
+
+# Function to create ``dual`` or ``dual_mpi`` command
+def dual(opts=None, i=0, **kw):
+    """Interface to FUN3D binary ``dual`` or ``dual_mpi``
+    
+    :Call:
+        >>> cmdi = cmd.dual(opts, i=0)
+        >>> cmdi = cmd.dual(**kw)
+    :Inputs:
+        *opts*: :class;`pyFun.options.Options`
+            Global pyFun options interface or "RunControl" interface
+        *i*: :class:`int`
+            Phase number
+        *outer_loop_krylov*: {``True``} | ``False``
+            Whether or not to use ``--outer_loop_krylov`` option
+        *rad*: {``True``} | ``False``
+            Whether or not to use residual adjoint dot product
+        *adapt*: {``True``} | ``False``
+            Whether or not to adapt at the end of the cycle
+    :Outputs:
+        *cmdi*: :class:`list` (:class:`str`)
+            Command split into a list of strings
+    :Versions:
+        * 2016-04-28 ``@ddalle``: First version
+    """
+    # Check for options input
+    if opts is not None:
+        # Downselect to "RunControl" section if necessary
+        if "RunControl" in opts:
+            opts = opts["RunControl"]
+        # Downselect to "dual" section if necessary
+        if "dual" in opts:
+            opts = opts["dual"]
+    else:
+        # Default
+        opts = runControl.dual()
+    # Get values for run configuration
+    n_mpi  = opts.get_MPI(i)
+    nProc  = opts.get_nProc(i)
+    mpicmd = opts.get_mpicmd(i)
+    # Process keyword overrides
+    n_mpi  = kw.get('MPI',    n_mpi)
+    nProc  = kw.get('nProc',  nProc)
+    mpicmd = kw.get('mpicmd', mpicmd)
+    # Process default options
+    olk   = opts.get_dual_outer_loop_krylov(i)
+    rad   = opts.get_dual_rad(i)
+    adapt = opts.get_dual_adapt(i)
+    # Process keyword overrides
+    olk   = kw.get('outer_loop_krylov', olk)
+    rad   = kw.get('rad',               rad)
+    adapt = kw.get('adapt',             adapt)
+    # Process other options
+    cli_dual = dict(**opts)
+    # Known flags
+    flags = ['MPI', 'nProc', 'mpicmd', 'outer_loop_krylov', 'rad', 'adapt']
+    # Remove known flags
+    for k in cli_dual:
+        # Remove it
+        if k in flags: del cli_dual[k]
+    # Append *kw* flags
+    for k in kw:
+        # Check if recognized
+        if k not in flags: cli_dual[k] = kw[k]
+    # Form the initial command
+    if n_mpi:
+        # Use the ``dual_mpi`` command
+        cmdi = [mpicmd, '-np', str(nProc), 'dual_mpi']
+    else:
+        # Use the serial ``dual`` command
+        cmdi = ['dual']
+    # Process known options
+    if adapt: cmdi.append('--adapt')
+    if rad:   cmdi.append('--rad')
+    if olk:   cmdi.append('--outer_loop_krylov')
+    # Loop through command-line inputs
+    for k in cli_dual:
+        # Get the value.
+        v = cli_dual[k]
+        # Check the type
+        if v == True:
+            # Just an option with no value
+            cmdi.append('--'+k)
+        elif v == False or v is None:
+            # Do not use.
+            pass
+        else:
+            # Append the option and value 
+            cmdi.append('--'+k)
+            cmdi.append(str(v))
+    # Output
+    return cmdi
+        
         
