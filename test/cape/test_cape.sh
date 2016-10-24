b78f1f6b50b76eb09e0430adf99e68acbb0005cd
Derek J. Dalle <derek.j.dalle@nasa.gov>
2016-10-24 10:39:40 -0700

    Finished testing tri file output

---
+++ test/cape/test_cape.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Function to run a test and check its status
+function test {
+    # Run the test
+    echo "Testing '$1'..."
+    python $1/test_$1.py > $1/test.out
+    # Check the status.
+    if [ -f $1/FAIL ]; then
+        echo "  FAIL"
+        exit 1
+    elif [ -f $1/PASS ]; then
+        echo "  PASS"
+    else
+        echo "  Undetermined status"
+        exit 2
+    fi
+}
+
+# Prepare the environment
+. $MODULESHOME/init/bash
+module load pycart
+module load cart3d
+module load cgt
+
+# Run the test
+test endian
+
