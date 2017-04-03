Fixed all issues with OVERFLOW viscous forces in TriqFM

cape/tri.py
@@ -5497,7 +5497,7 @@ class Triq(TriBase):
             VL = np.mean(Q[T[IV,:],8], axis=1)
             WL = np.mean(Q[T[IV,:],9], axis=1)
             # Sheer stress multiplier
-            FTMUJ = 2.0*mu*REI/VOL[IV]
+            FTMUJ = mu*REI/VOL[IV]
             # Stress flux
             ZUVW = (1.0/3.0) * (VAX*UL + VAY*VL + VAZ*WL)
             # Stress tensor