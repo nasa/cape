Performed bug fixes for OVERFLOW/TriqFM to write PLT files with cf_x instead of native states

cape/plt.py
@@ -486,14 +486,10 @@ class Plt(object):
         except Exception:
             # No extra states
             nq = 0
-        # Set number of variables
-        self.nVar = 3 + nq
         # Process variables
         qvars = kw.get("vars", kw.get("Vars"))
         # Process default
         if qvars is None:
-            # Set a flag for doing anything special
-            qcf = False
             # Process default list based on number of states
             if nq == 1:
                 # Pressure coefficient
@@ -514,15 +510,24 @@ class Plt(object):
                 try:
                     # Try to calculate skin friction
                     cf_x, cf_y, cf_z = triq.GetSkinFriction(comp=CompIDs, **kw)
+                    # Get nodes affected
+                    I = triq.GetNodesFromCompID(CompIDs)
                     # Get the key states
                     qvars = [
                         "cp",
                         "rho", "rhou", "rhov", "rhow", "e",
                         "cf_x", "cf_y", "cf_z"
                     ]
-                    # Doing something special
-                    qcf = True
-                except Exception:
+                    # Downsize state variables
+                    triq.q = triq.q[:,:9]
+                    # Update number of states
+                    triq.nq = 9
+                    nq = triq.nq
+                    # Save the skin friction.
+                    triq.q[I,6] = cf_x
+                    triq.q[I,7] = cf_y
+                    triq.q[I,8] = cf_z
+                except Exception as e:
                     # Fall back to the native states
                     qvars = [
                         "cp",
@@ -600,12 +605,6 @@ class Plt(object):
             T = cape.util.TrimUnused(Tris) - 1
             # Form the state matrix for this zone
             q = np.hstack((Nodes, triq.q[I,:self.nVar]))
-            # Check for overwrite
-            if qcf:
-                # Save the skin friction.
-                q[:,6] = cf_x
-                q[:,7] = cf_y
-                q[:,8] = cf_z
             # Save the min/max
             self.qmin[n,:] = np.min(q, axis=0)
             self.qmax[n,:] = np.max(q, axis=0)

cape/tri.py
@@ -5390,6 +5390,8 @@ class Triq(TriBase):
         v0 = T[:,0]
         v1 = T[:,1]        
         v2 = T[:,2]
+        # Handle to state variables
+        Q = self.q
         # Extract the vertices of each tri.
         x = self.Nodes[T, 0]
         y = self.Nodes[T, 1]
@@ -5458,33 +5460,35 @@ class Triq(TriBase):
         # Initialize viscous forces
         Fv = np.zeros((nTri, 3))
         # Save results from non-zero volumes
-        Fv[IV,0] = (TXX*VAX + TXY*VAY + TXZ*VAZ)
-        Fv[IV,1] = (TXY*VAX + TYY*VAY + TYZ*VAZ)
-        Fv[IV,2] = (TXZ*VAX + TYZ*VAY + TZZ*VAZ)
-        # Normalize
-        Fv /= A
+        Fv[IV,0] = (TXX*VAX + TXY*VAY + TXZ*VAZ)/A
+        Fv[IV,1] = (TXY*VAX + TYY*VAY + TYZ*VAZ)/A
+        Fv[IV,2] = (TXZ*VAX + TYZ*VAY + TZZ*VAZ)/A
         # Initialize friction coefficients
-        cf_x = np.zeros(nNode)
-        cf_y = np.zeros(nNode)
-        cf_z = np.zeros(nNode)
+        cf_x = np.zeros(self.nNode)
+        cf_y = np.zeros(self.nNode)
+        cf_z = np.zeros(self.nNode)
         # Initialize areas
-        Af = np.zeros(nNode)
+        Af = np.zeros(self.nNode)
         # Add friction values weighted by areas
-        cf_x[T[:,0]] += (Fv[:,0] * A[K])
-        cf_x[T[:,1]] += (Fv[:,0] * A[K])
-        cf_x[T[:,2]] += (Fv[:,0] * A[K])
-        cf_y[T[:,0]] += (Fv[:,1] * A[K])
-        cf_y[T[:,1]] += (Fv[:,1] * A[K])
-        cf_y[T[:,2]] += (Fv[:,1] * A[K])
-        cf_z[T[:,0]] += (Fv[:,2] * A[K])
-        cf_z[T[:,1]] += (Fv[:,2] * A[K])
-        cf_z[T[:,2]] += (Fv[:,2] * A[K])
+        cf_x[T[:,0]] += (Fv[:,0] * A)
+        cf_x[T[:,1]] += (Fv[:,0] * A)
+        cf_x[T[:,2]] += (Fv[:,0] * A)
+        cf_y[T[:,0]] += (Fv[:,1] * A)
+        cf_y[T[:,1]] += (Fv[:,1] * A)
+        cf_y[T[:,2]] += (Fv[:,1] * A)
+        cf_z[T[:,0]] += (Fv[:,2] * A)
+        cf_z[T[:,1]] += (Fv[:,2] * A)
+        cf_z[T[:,2]] += (Fv[:,2] * A)
         # Accumulate areas
-        Af[T[:,0]] += A[K]
-        Af[T[:,1]] += A[K]
-        Af[T[:,2]] += A[K]
+        Af[T[:,0]] += A
+        Af[T[:,1]] += A
+        Af[T[:,2]] += A
+        # Downselect
+        cf_x = cf_x[I] / Af[I]
+        cf_y = cf_y[I] / Af[I]
+        cf_z = cf_z[I] / Af[I]
         # Output
-        return cf_x/Af, cf_y/Af, cf_z/Af
+        return cf_x, cf_y, cf_z
             
             
     # Calculate forces and moments