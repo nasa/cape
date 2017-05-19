b4d896c224f2e5c7201fd21919bc01d2446a1f24
Derek J. Dalle <derek.j.dalle@nasa.gov>
2017-05-19 10:56:56 -0700

    Not creating empty triqfm folder anymore

--- cape/dataBook.py
+++ cape/dataBook.py
@@ -3977,10 +3977,10 @@ class DBTriqFM(DataBook):
         os.chdir(self.RootDir)
         # Get databook dir and triqfm dir
         fdir = self.opts.get_DataBookDir()
-        #ftrq = os.path.join(fdir, 'triqfm')
+        ftrq = os.path.join(fdir, 'triqfm')
         # Ensure folder exists
         if not os.path.isdir(fdir): self.opts.mkdir(fdir)
-        if not os.path.isdir(ftrq): self.opts.mkdir(ftrq)
+        #if not os.path.isdir(ftrq): self.opts.mkdir(ftrq)
         # Loop through patches
         for patch in ([None] + self.patches):
             # Sort it.
