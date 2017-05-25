9d78f6168d7f9d1d9a517e123f24b12923c0c387
Derek J. Dalle <derek.j.dalle@nasa.gov>
2017-05-25 13:48:50 -0700

    Fixed example grid system for pyOver

--- examples/cad/bullet/prepBullet.sh
+++ examples/cad/bullet/prepBullet.sh
@@ -6,5 +6,8 @@ module load pycart
 
 # Convert to ASCII tri
 pc_UH3D2Tri.py -i bullet.uh3d -c bullet.xml
-pc_UH3D2Tri.py -i bullet.uh3d -b4
+pc_UH3D2Tri.py -i bullet.uh3d -lb4
+
+# Generate curves
+pc_StepTri2Crv.py bullet -o bullet.lb8.crv -lb8
 
