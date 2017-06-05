4c4dd1b30553b66cfd52fc30ecc080fba4489fbe
Derek J. Dalle <derek.j.dalle@nasa.gov>
2017-06-05 12:00:16 -0700

    Slightly more helpful JSON syntax error messages

--- cape/options/util.py
+++ cape/options/util.py
@@ -421,8 +421,8 @@ def loadJSONFile(fname):
                 # Not useful; split into list of lines
                 lines = lines.split('\n')
             # Start and end line number
-            n0 = max(n-2, 0)
-            n1 = min(n+1, len(lines))
+            n0 = max(n-3, 0)
+            n1 = min(n+2, len(lines))
             # Initialize message with 
             msg = "Error while reading JSON file '%s':\n" % fname
             # Add the exception's message
@@ -431,7 +431,12 @@ def loadJSONFile(fname):
             # Print some lines around the problem
             for i in range(n0, n1):
                 # Add line with line number
-                msg += ("%4i: %s\n" % (i+1, lines[i]))
+                if i+1 == n:
+                    # Add special marker for reported line
+                    msg += ("%4i> %s\n" % (i+1, lines[i]))
+                else:
+                    # Neighboring line
+                    msg += ("%4i: %s\n" % (i+1, lines[i]))
             # Show the message
             raise ValueError(msg)
         except ValueError as e:
