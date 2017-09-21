dc8f1d522adb141879d5a9e1afd7b59634184ec3
Derek J. Dalle <derek.j.dalle@nasa.gov>
2017-09-21 11:45:58 -0700

    Finally debugged FUN3D boundary points with help of fun3d-support
    I was missing an 's' in sampling_parameters>points

--- pyFun/fun3d.py
+++ pyFun/fun3d.py
@@ -1459,14 +1459,11 @@ class Fun3d(Cntl):
                 'boundary_points', ngeom)
             # Set number of points
             nml.SetVar('sampling_parameters', 'number_of_points', npt, ngeom)
-            # Set boundary point variable list
-            nml.SetVar('sampling_parameters', 'variable_list',
-                'x,y,z,cp', ngeom)
             # Loop through points
             for j in range(1,npt+1):
                 # Set point
-                nml.SetVar('sampling_parameters', 'point',
-                    PS[j-1], ("1:3", ngeom, j))
+                nml.SetVar('sampling_parameters', 'points',
+                    PS[j-1], (":", ngeom, j))
         # Set number of geometries
         nml.SetVar('sampling_parameters', 'number_of_geometries', ngeom)
    # ]
