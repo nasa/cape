17147597c934578bc227315b40aad67ca01e49e3
Derek J. Dalle <derek.j.dalle@nasa.gov>
2019-06-20 13:31:45 -0700

    Introduced possibility of removing *two* edges of certain small triangles

--- cape/tri.py
+++ cape/tri.py
@@ -4440,8 +4440,9 @@ class TriBase(object):
             else:
                 # No double-neighbors
                 continue
-            # If we have double neighbors, remove those edges, too
-            J = np.hstack((J, Ji))
+            # If we have double neighbors, remove the shorter of these
+            J = np.hstack((J, [Ji[np.argmin(D[i,Ji])]]))
+            I = np.vstack((I, I[[i]]))
         # Combine start/end node indices
         I0 = np.array([I[i][[j, (j + 1) % 3]] for (i, j) in enumerate(J)])
         # Sort the node pairs so we mark the same node for deletion
