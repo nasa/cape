e88d1d3ef9a92d2c98b89546d67b9c138ce6268a
Derek Dalle <derek.dalle@gmail.com>
2019-07-05 03:38:32 -0700

    More progress on test driver

--- cape/testutils/driver.py
+++ cape/testutils/driver.py
@@ -27,6 +27,7 @@ options to the test crawler.
 
 # Standard library modules
 import os
+import time
 import shutil
 
 # Local modules
@@ -76,6 +77,62 @@ class TestDriver(object):
     
     
     # Representation method
+    
+    
+    # Execute test
+    def run_commands(self):
+        """Execute tests in the current folder
+        
+        :Call:
+            >>> statuses = driver.run_commands()
+        :Inputs:
+            *driver*: :class:`cape.testutils.driver.TestDriver`
+                Test driver controller
+        :Outputs:
+        :Versions:
+            * 2019-07-03 ``@ddalle``: First version
+        """
+        # Get commands to run
+        cmds = self.opts.get("Commands", [])
+        # Get output file names
+        fnout = self.opts.get("stdout", "STDOUT")
+        fnerr = self.opts.get("stderr", "STDERR")
+        # Maximum allowed time
+        
+        # Open output files
+        if fnout is None:
+            # No output file
+            fout = None
+        elif isinstance(fnout, int):
+            # Identifier; leave as is
+            fout = fnout
+        else:
+            # Open file
+            fout = open(fnout, 'w')
+        # Open STDERR file
+        if fnerr is None:
+            # No error file
+            ferr = None
+        elif isinstance(fnerr, int):
+            # Identifier; leave as is
+            ferr = fnerr
+        elif fnerr == fnout:
+            # Directing both streams to same file
+            ferr = fout
+        else:
+            # Open STDERR file
+            ferr = open(fnerr, 'w')
+        # Loop through commands
+        for i, cmd in enumerate(cmds):
+            # Call the command 
+            t, ierr, out, err = testshell.comm(
+                cmd, maxtime=tmax, dt=dt, stdout=stdout, stderr=stderr)
+        # Close files
+        if isinstance(fout, file):
+            fout.close()
+        if isinstance(ferr, file):
+            ferr.close()
+            
     
     
     # Prepare a test
