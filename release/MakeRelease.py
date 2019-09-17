cb254768a7babdebdeefcc038a14c609d68c4893
Derek J. Dalle <derek.j.dalle@nasa.gov>
2019-09-17 11:31:02 -0700

    Created an updated release script that seems to work well

--- release/MakeRelease.py
+++ release/MakeRelease.py
@@ -4,121 +4,207 @@ Create a release tar ball for Cape
 
 :Versions:
     * 2015-10-29 ``@ddalle``: Started
+    * 2019-09-16 ``@ddalle``: Update for version 0.9
 """
 
-# System interface
-import os, shutil, fnmatch
+# Standard library modules
+import os
+import sys
+import shutil
+import fnmatch
 import subprocess as sp
+
 # The module itself
-import cape, cape.argread
+import cape
+import cape.argread
 
 # Permissions to use for new folders
 dmask = 0o755
 
+
 # Make function
 def MakeRelease(ver):
-    # Go to home directory.
+    # Remember current folder name
     fpwd = os.getcwd()
+    # Go to parent directory of entire git repo
     os.chdir(cape.CapeFolder)
     os.chdir('..')
-    # Folder name
-    fdir = 'cape_%s' % ver
-    # Make the release folder.
+
+    # Create zip for the main content
+    ZipModule(ver)
+    # Create zip of HTML documentation
+    ZipHTMLDoc(ver)
+    # Zip each example
+    ZipExamples(ver)
+
+
+# Create the ``.zip`` for the main content
+def ZipModule(ver):
+    # Release name
+    fdir = 'cape-%s' % ver
+    # Check if the folder already exists
     if os.path.isdir(fdir):
-        raise SystemError("Folder '%s' already exists" % fdir)
+        raise SystemError(
+            "Container folder for release '%s' already exists" % fdir)
+    # Make the release folder
     os.mkdir(fdir, 0o755)
     
+    # Enter documentation folder
+    os.chdir("doc")
+    # Compile PDF documentation
+    ierr = os.system("make latexpdf")
+    # Check for error
+    if ierr:
+        raise SystemError("Error during `make latexpdf` execution")
+    # Return
+    os.chdir("..")
+
     # Files to ignore for modules
-    modignore = ['*.py[oc]']
+    modignore = [
+        "*.py[oc]",
+        "__pycache__",
+        "*.so",
+        "pyUS*",
+        "pyus*",
+        "us3d"
+    ]
     # Copy the modules.
     CopyTree('cape',   fdir, modignore, depth=2)
     CopyTree('pyCart', fdir, modignore, depth=2)
     CopyTree('pyFun',  fdir, modignore, depth=2)
     CopyTree('pyOver', fdir, modignore, depth=2)
     
-    # Other settings
+    # Other settings and tools
     CopyTree('bin',         fdir, modignore, depth=2)
     CopyTree('settings',    fdir, modignore, depth=2)
     CopyTree('modulefiles', fdir, modignore, depth=2)
+    CopyTree('templates',   fdir, modignore, depth=2)
     
     # Installation files
     shutil.copy('Makefile',   fdir)
     shutil.copy('config.cfg', fdir)
+    # Manual
+    shutil.copy(os.path.join("doc", "_build", "latex", "cape.pdf"),
+        os.path.join(fdir, "CAPE-manual-%s.pdf" % ver))
     
-    # Files to ignore for documentation
-    docignore = ['_*']
+    # Name of the zip file
+    fzip = os.path.join('release', fdir+'.zip')
+    # Delete zip file if it exists
+    if os.path.isfile(fzip):
+        os.remove(fzip)
+    # Zip the folder
+    sp.call(['zip', fzip, '-r', fdir])
+    # Delete folder
+    shutil.rmtree(fdir)
+    
+    
+# Create the ``.zip`` for the HTML documentation
+def ZipHTMLDoc(ver):
+    # Release name
+    fdir = 'cape-%s' % ver
+    # Check if the folder already exists
+    if os.path.isdir(fdir):
+        raise SystemError(
+            "Container folder for release '%s' already exists" % fdir)
+    # Make the release folder
+    os.mkdir(fdir, 0o755)
+    
+    # Enter documentation folder
+    os.chdir("doc")
+    # Compile PDF documentation
+    ierr = os.system("make html")
+    # Check for error
+    if ierr:
+        raise SystemError("Error during `make latexpdf` execution")
+    # Return
+    os.chdir("..")
+    
+    
     # Documentation folder to copy
-    fdoc = os.path.join('doc')
+    fdoc = os.path.join("doc", "_build", "html")
     # Copy the documentation
-    CopyTree(fdoc, os.path.join(fdir, 'doc'), docignore, depth=16)
+    CopyTree(fdoc, os.path.join(fdir, "doc"), [], depth=16)
+    
+    # Destination tar
+    fzip = os.path.join('release', fdir+'-HTML-doc.zip')
+    # Delete zip file if it exists
+    if os.path.isfile(fzip):
+        os.remove(fzip)
+    # Zip the folder
+    sp.call(['zip', fzip, '-r', fdir])
+    # Remove the folder
+    shutil.rmtree(fdir)
+
+
+# Tar examples
+def ZipExamples(ver):
+    # Release name
+    fdir = 'cape-%s' % ver
+    # Check if the folder already exists
+    if os.path.isdir(fdir):
+        raise SystemError(
+            "Container folder for release '%s' already exists" % fdir)
+    # Make the release folder
+    os.mkdir(fdir, 0o755)
+    # Create destination folders for examples
+    os.mkdir(os.path.join(fdir, 'examples'), 0o755)
+    os.mkdir(os.path.join(fdir, 'examples', 'pycart'), 0o755)
+    os.mkdir(os.path.join(fdir, 'examples', 'pyfun'),  0o755)
+    os.mkdir(os.path.join(fdir, 'examples', 'pyover'), 0o755)
+    
+    # List of examples
+    fex_list = [
+        os.path.join("pycart", "01_bullet"),
+        os.path.join("pycart", "02_arrow"),
+        os.path.join("pycart", "03_fins"),
+        os.path.join("pycart", "04_bJet"),
+        os.path.join("pycart", "05_adapt_bJet"),
+        os.path.join("pycart", "06_lineload_arrow"),
+        os.path.join("pycart", "07_data_arrow"),
+        os.path.join("pycart", "08_thrust"),
+        os.path.join("pyfun",  "01_arrow"),
+        os.path.join("pyfun",  "02_bullet"),
+        os.path.join("pyover", "01_bullet"),
+    ]
     
     # Files to ignore for examples
     exignore = [
-        'powerof*', 'poweron', 'invisc', 'batch-pbs', 'report-*',
-        "*.ugrid", "arrow"
+        "powerof*",
+        "poweron",
+        "invisc",
+        "batch-pbs",
+        "report-*",
+        "*.ugrid",
+        "arrow"
     ]
-    # Create destination folders for examples
-    os.mkdir(os.path.join(fdir, 'examples'), dmask)
-    os.mkdir(os.path.join(fdir, 'examples', 'pycart'), dmask)
-    os.mkdir(os.path.join(fdir, 'examples', 'pyfun'),  dmask)
-    os.mkdir(os.path.join(fdir, 'examples', 'pyover'), dmask)
-    # pyCart Examples folder
-    fexi = os.path.join('examples', 'pycart')
-    fexo = os.path.join(fdir, 'examples', 'pycart')
-    # Copy each example individually.
-    CopyTree(os.path.join(fexi, '01_bullet'),         fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '02_arrow'),          fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '03_fins'),           fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '04_bJet'),           fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '05_adapt_bJet'),     fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '06_lineload_arrow'), fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '07_data_arrow'),     fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '08_thrust'),         fexo, exignore, depth=2)
-    # pyFun Examples folder
-    fexi = os.path.join('examples', 'pyfun')
-    fexo = os.path.join(fdir, 'examples', 'pyfun')
-    # Copy each example individually.
-    CopyTree(os.path.join(fexi, '01_arrow'),          fexo, exignore, depth=2)
-    CopyTree(os.path.join(fexi, '02_bullet'),         fexo, exignore, depth=2)
-    # pyOver Examples folder
-    fexi = os.path.join('examples', 'pyover')
-    fexo = os.path.join(fdir, 'examples', 'pyover')
-    # Copy each example individually.
-    CopyTree(os.path.join(fexi, '01_bullet'),         fexo, exignore, depth=3)
     
-    # Destination tar
-    fzip = os.path.join('release', fdir+'.zip')
-    fdoc = os.path.join('release', fdir+'_doc.zip')
-    fsam = os.path.join('release', fdir+'_examples.zip')
-    # Check for the tar ball
-    for f in [fzip, fdoc, fsam]:
-        if os.path.isfile(f):
-            raise SystemError("Release '%s' exists; aborting" % f)
-    # Subfolders to zip
-    fzmain = [                    
-        os.path.join(fdir, 'bin'),
-        os.path.join(fdir, 'cape'),
-        os.path.join(fdir, 'config.cfg'),
-        os.path.join(fdir, 'Makefile'),
-        os.path.join(fdir, 'pyCart'),
-        os.path.join(fdir, 'pyFun'),
-        os.path.join(fdir, 'pyOver'),
-        os.path.join(fdir, 'modulefiles'),
-        os.path.join(fdir, 'settings'),
-    ]
-    fzdoc = os.path.join(fdir, 'doc')
-    fzsam = os.path.join(fdir, 'examples') 
-    # Zip the folder
-    sp.call(['zip', fzip, '-r'] + fzmain)
-    sp.call(['zip', fdoc, '-r', fzdoc])
-    sp.call(['zip', fsam, '-r', fzsam])
-    # Remove the folder
+    # Loop through examples
+    for fex in fex_list:
+        # Full path to example source files and copy destination
+        fexi = os.path.join("examples", fex)
+        fexo = os.path.join(fdir, fexi)
+        # Make folder
+        os.mkdir(fexo, 0o755)
+        
+        # Copy files
+        CopyTree(fexi, fexo, exignore, depth=2)
+        
+        # Example name modified for single file name
+        fexz = fdir + "-" + fex.replace(os.sep, "-") + ".zip"
+        # Destination tar
+        fzip = os.path.join('release', fexz) 
+        # Delete zip file if it exists
+        if os.path.isfile(fzip):
+            os.remove(fzip)
+        # Zip the folder
+        sp.call(['zip', fzip, '-r', fexo])
+        # Remove the folder
+        shutil.rmtree(fexo)
+    
+    # Clear release directory
     shutil.rmtree(fdir)
-    
-    # Return to original location
-    os.chdir(fpwd)
-# def MakeRelease
-    
+
+
 # Copy a folder using a glob
 def CopyTree(fsrc, fdst, ignore=[], depth=2):
     """Copy a folder while ignoring a list of globs
@@ -150,9 +236,11 @@ def CopyTree(fsrc, fdst, ignore=[], depth=2):
         fsrci = os.path.split(os.path.abspath(fsrc))[1]
         fdsti = os.path.split(os.path.abspath(fdst))[1]
         # Check for non-matching names
-        if fdsti != fsrci: fdst = os.path.join(fdst, fsrci)
-        # Create folder if necessary.
-        if not os.path.isdir(fdst): os.mkdir(fdst, dmask)
+        if fdsti != fsrci:
+            fdst = os.path.join(fdst, fsrci)
+        # Create folder if necessary
+        if not os.path.isdir(fdst):
+            os.mkdir(fdst, dmask)
         
     # Loop through files
     for fname in os.listdir(fsrc):
@@ -185,13 +273,13 @@ def CopyTree(fsrc, fdst, ignore=[], depth=2):
         else:
             # Copy the file
             shutil.copy(frel, fdst)
-# def CopyTree
+
 
 
 # Check if called as script
 if __name__ == "__main__":
     # Read arguments
-    a, kw = cape.argread.readflags(os.sys.argv)
+    a, kw = cape.argread.readflags(sys.argv)
     # Check for version name
     if len(a) > 0:
         # Version specified
