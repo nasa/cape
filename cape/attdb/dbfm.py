e04071f5a70d244bc3b329f43fdf44ffd1838f8a
Derek Dalle <derek.dalle@gmail.com>
2020-02-18 22:04:41 -0800

    Start (finally) finale attdb.dbfm module
    Considering a final naming upgrade:
        * Consolidate all of the rdb modules into one (quite large)
        * Rename the one big database class DataKit
    
    The idea is that a data kit is a database plus a response/call
    methodology plus possibly more stuff.  Somehow the module name rdb is still
    appealing compared to dkit or datakit.

---
+++ cape/attdb/dbfm.py
@@ -0,0 +1,37 @@
+#!/us/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+-----------------------------------------------------------------
+:mod:`cape.attdb.dbfm`: Aero Task Team Force & Moment Databases
+-----------------------------------------------------------------
+
+This module provides customizations of :mod:`cape.attdb.rdb` that are
+especially useful for launch vehicle force & moment databases.  The
+force & moment coefficient names follow common missile naming
+conventions:
+
+    =======  ===============  ============================
+    Name     Symbol           Description
+    =======  ===============  ============================
+    *CA*     :math:`C_A`      Axial force
+    *CY*     :math:`C_Y`      Side force
+    *CN*     :math:`C_N`      Normal force
+    *CLL*    :math:`C_\ell`   Rolling moment
+    *CLM*    :math:`C_m`      Pitching moment
+    *CLN*    :math:`C_n`      Yawing moment
+    =======  ===============  ============================
+
+
+"""
+
+# Standard library modules
+import os
+import sys
+
+# Third-party modules
+import numpy as np
+
+# Local modules
+from . import rdbscalar
+
+
