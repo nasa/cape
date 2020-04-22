75d87ab6e0cb5a771f039499d8b7626e8bda5da1
Derek Dalle <derek.dalle@gmail.com>
2020-04-22 09:30:09 -0700

    Add options for 'us3d-prepar'

--- cape/pyus/options/runControl.py
+++ cape/pyus/options/runControl.py
@@ -49,7 +49,159 @@ import cape.cfdx.options.runControl
 from .Archive import Archive
 
 # Class for inputs to the US3D executable
+class US3D(odict):
+    pass
 
+
+# Class for inputs to the ``us3d-prepar`` executable
+class US3DPrepar(odict):
+    # Option to run ``us3d-prepar``
+    def get_us3d_prepar_run(self, i=0):
+        r"""Get option to run or not run ``us3d-prepar``
+
+        :Call:
+            >>> run = opts.get_us3d_prepar_run(i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *i*: {``0``} | :class:`int` | ``None``
+                Phase number
+        :Outputs:
+            *run*: {``True``} | ``False``
+                Option to run or not run ``us3d-prepar``
+        :Versions:
+            * 2020-04-22 ``@ddalle``: First version
+        """
+        return self.get_key("run", i, rck="us3d_prepar_run")
+
+    # Option to run ``us3d-prepar``
+    def set_us3d_prepar_run(self, run=rc0("us3d_prepar_run"), i=0):
+        r"""Set option to run or not run ``us3d-prepar``
+
+        :Call:
+            >>> opts.set_us3d_prepar_run(run=True, i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *run*: {``True``} | ``False``
+                Option to run or not run ``us3d-prepar``
+            *i*: {``0``} | :class:`int` | ``None``
+                Phase number
+        :Versions:
+            * 2020-04-22 ``@ddalle``: First version
+        """
+        self.set_key("run", run, i)
+
+    # Name of grid input file
+    def get_us3d_prepar_grid(self, i=None):
+        r"""Get name of input grid to ``us3d-prepar``
+
+        :Call:
+            >>> grid = opts.get_us3d_prepar_grid(i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *i*: :class:`int`
+                Phase number
+        :Outputs:
+            *grid*: :class:`str`
+                Fluent-format input mesh file name
+        :Versions:
+            * 2020-04-21 ``@ddalle``: First version
+        """
+        return self.get_key("grid", i, rck="us3d_prepar_grid")
+
+    # Name of grid input file
+    def set_us3d_prepar_grid(self, grid=rc0("us3d_prepar_grid"), i=None):
+        r"""Set name of input grid to ``us3d-prepar``
+
+        :Call:
+            >>> opts.set_us3d_prepar_grid(grid, i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *grid*: :class:`str`
+                Fluent-format input mesh file name
+            *i*: :class:`int`
+                Phase number
+        :Versions:
+            * 2020-04-21 ``@ddalle``: First version
+        """
+        self.set_key("grid", grid, i)
+
+    # Name of grid input file
+    def get_us3d_prepar_conn(self, i=None):
+        r"""Get name of connectivity file made by ``us3d-prepar``
+
+        :Call:
+            >>> conn = opts.get_us3d_prepar_conn(i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *i*: :class:`int`
+                Phase number
+        :Outputs:
+            *conn*: :class:`str`
+                Name of HDF5 file for US3D mesh connectivity
+        :Versions:
+            * 2020-04-22 ``@ddalle``: First version
+        """
+        return self.get_key("conn", i, rck="us3d_prepar_conn")
+
+    # Name of grid input file
+    def set_us3d_prepar_conn(self, conn=rc0("us3d_prepar_conn"), i=None):
+        r"""Set name of connectivity file made by ``us3d-prepar``
+
+        :Call:
+            >>> opts.set_us3d_prepar_conn(conn, i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *conn*: :class:`str`
+                Name of HDF5 file for US3D mesh connectivity
+            *i*: :class:`int`
+                Phase number
+        :Versions:
+            * 2020-04-22 ``@ddalle``: First version
+        """
+        self.set_key("conn", conn, i)
+
+    # Name of grid input file
+    def get_us3d_prepar_out(self, i=None):
+        r"""Get name of mesh file converted by ``us3d-prepar``
+
+        :Call:
+            >>> out = opts.get_us3d_prepar_out(i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *i*: :class:`int`
+                Phase number
+        :Outputs:
+            *out*: :class:`str`
+                Name of HDF5 mesh file for US3D
+        :Versions:
+            * 2020-04-22 ``@ddalle``: First version
+        """
+        return self.get_key("out", i, rck="us3d_prepar_out")
+
+    # Name of grid input file
+    def set_us3d_prepar_out(self, out=rc0("us3d_prepar_out"), i=None):
+        r"""Get name of mesh file converted by ``us3d-prepar``
+
+        :Call:
+            >>> out = opts.get_us3d_prepar_out(i=None)
+        :Inputs:
+            *opts*: :class:`cape.pyus.options.Options`
+                Options interface
+            *out*: :class:`str`
+                Name of HDF5 mesh file for US3D
+            *i*: :class:`int`
+                Phase number
+        :Versions:
+            * 2020-04-22 ``@ddalle``: First version
+        """
+        self.set_key("out", out, i)
 
 
 # Class for Report settings
@@ -70,43 +222,115 @@ class RunControl(cape.cfdx.options.runControl.RunControl):
         # Upgrade important groups to their own classes.
         self._Environ()
         self._ulimit()
-        self._aflr3()
-        self._intersect()
-        self._verify()
+        self._US3DPrepar()
         self._Archive()
         
    # ============ 
    # Initializers
    # ============
    # <
-   
+    # Initialization and confirmation for ``us3d-prepar`` options
+    def _US3DPrepar(self):
+        """Initialize ``us3d-prepar`` options if necessary"""
+        # Get object
+        opts = self.get("us3d-prepar", {})
+        # Check types
+        if isinstance(opts, US3DPrepar):
+            # Nothing to do
+            pass
+        elif isinstance(opts, dict):
+            # Convert to special class
+            self["us3d-prepar"] = US3DPrepar(**opts)
+        elif opts:
+            # Create empty class with defaults
+            self["us3d-prepar"] = US3DPrepar(run=True)
+        else:
+            # Create empty class, ``us3d-prepar`` turned off
+            self["us3d-prepar"] = US3DPrepar(run=False)
+
     # Initialization and confirmation for options to US3D executable
-            
+
+
+    # Initialization method for folder management options
+    def _Archive(self):
+        """Initialize folder management options if necessary"""
+        # Get object
+        opts = self.get("Archive", {})
+        # Check type
+        if isinstance(opts, Archive):
+            # Nothing to do
+            pass
+        elif isinstance(opts, dict):
+            # Convert
+            self["Archive"] = Archive(**opts)
+        else:
+            # Bad type
+            raise TypeError('"Archive" option has bad type')
    # >
    
    # ============== 
    # Local settings
    # ==============
    # <
-   
-        
+   # >
+
+   # =============
+   # us3d-prepar
+   # =============
+   # <
+    # Option to run
+    def get_us3d_prepar_run(self, i=0):
+        self._US3DPrepar()
+        return self["us3d-prepar"].get_us3d_prepar_run(i=i)
+
+    # Option to run
+    def set_us3d_prepar_run(self, run=rc0("us3d_prepar_run"), i=0):
+        self._US3DPrepar()
+        self["us3d-prepar"].set_us3d_prepar_run(run, i=i)
+
+    # Option for input file
+    def get_us3d_prepar_grid(self, i=None):
+        self._US3DPrepar()
+        return self["us3d-prepar"].get_us3d_prepar_grid(i=i)
+
+    # Option for input file
+    def set_us3d_prepar_grid(self, grid=rc0("us3d_prepar_grid"), i=None):
+        self._US3DPrepar()
+        self["us3d-prepar"].set_us3d_prepar_grid(grid, i=i)
+
+    # Option for conn file
+    def get_us3d_prepar_conn(self, i=None):
+        self._US3DPrepar()
+        return self["us3d-prepar"].get_us3d_prepar_conn(i=i)
+
+    # Option for conn file
+    def set_us3d_prepar_conn(self, conn=rc0("us3d_prepar_conn"), i=None):
+        self._US3DPrepar()
+        self["us3d-prepar"].set_us3d_prepar_conn(conn, i=i)
+
+    # Option for grid file
+    def get_us3d_prepar_out(self, i=None):
+        self._US3DPrepar()
+        return self["us3d-prepar"].get_us3d_prepar_out(i=i)
+
+    # Option for grid file
+    def set_us3d_prepar_out(self, out=rc0("us3d_prepar_out"), i=None):
+        self._US3DPrepar()
+        self["us3d-prepar"].set_us3d_prepar_out(out, i=i)
+
+    # Copy documentation     
+    for k in ["run", "grid", "conn", "out"]:
+        n1 = "get_us3d_prepar_" + k
+        n2 = "set_us3d_prepar_" + k
+        eval(n1).__doc__ = getattr(US3DPrepar, n1).__doc__
+        eval(n2).__doc__ = getattr(US3DPrepar, n2).__doc__
    # >
     
    # =================
    # Folder management
    # =================
    # <
-    # Initialization method for folder management options
-    def _Archive(self):
-        """Initialize folder management options if necessary"""
-        # Check status
-        if 'Archive' not in self:
-            # Missing entirely.
-            self['Archive'] = Archive()
-        elif type(self['Archive']).__name__ == 'dict':
-            # Convert to special class
-            self['Archive'] = Archive(**self['Archive'])
-    
+
    # >
 # class RunControl
 
