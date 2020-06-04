3c29879bf2a107cde13e54043da79f82bab3db47
Derek Dalle <derek.dalle@gmail.com>
2020-06-04 12:27:22 -0700

    Implement DBLL.genr8_ll3x_basis() for line load increment bases

--- cape/attdb/dbll.py
+++ cape/attdb/dbll.py
@@ -46,6 +46,7 @@ import numpy as np
 # CAPE modules
 import cape.attdb.convert as convert
 import cape.tnakit.kwutils as kwutils
+import cape.tnakit.typeutils as typeutils
 
 # Local modules
 from . import dbfm
@@ -161,7 +162,7 @@ class DBLL(dbfm.DBFM):
         r"""Make a moment line load based on a force line load
 
         :Call:
-            >>> v = db.make_ll_moment(col, ax1, ax2, xcol=None, **kw)
+            >>> v = db.make_ll_moment(col, ax1, ax2, ocol=None, **kw)
         :Inputs:
             *db*: :class:`cape.attdb.rdb.DataKit`
                 Database with analysis tools
@@ -306,7 +307,7 @@ class DBLL(dbfm.DBFM):
         r"""Create a moment line load based on a force line load
 
         :Call:
-            >>> v = db.create_ll_moment(col, ax1, ax2, xcol=None, **kw)
+            >>> v = db.create_ll_moment(col, ax1, ax2, ocol=None, **kw)
         :Inputs:
             *db*: :class:`cape.attdb.rdb.DataKit`
                 Database with analysis tools
@@ -448,12 +449,66 @@ class DBLL(dbfm.DBFM):
         r"""Create a moment line load based on a force line load
 
         :Call:
-            >>> v = db.genr8_ll_moment(col, ax1, ax2, xcol=None, **kw)
+            >>> v = db.genr8_ll_moment(col, ax1, ax2, **kw)
         :Inputs:
             *db*: :class:`cape.attdb.rdb.DataKit`
                 Database with analysis tools
             *col*: :class:`str`
                 Name of data column to integrate
+            *ax1*: ``0`` | ``1`` | ``2`` | ``"x"`` | ``"y"`` | ``"z"``
+                Force axis direction
+            *ax2*: ``0`` | ``1`` | ``2`` | ``"x"`` | ``"y"`` | ``"z"``
+                Moment axis direction
+            *xcol*: {``None``} | :class:`str`
+                Name of column to use as *x*-coords for moment arm
+            *ycol*: {``None``} | :class:`str`
+                Name of column to use as *y*-coords for moment arm
+            *zcol*: {``None``} | :class:`str`
+                Name of column to use as *z*-coords for moment arm
+            *x*: {``None``} | :class:`np.ndarray`
+                Explicit *x*-coords for moment arm
+            *y*: {``None``} | :class:`np.ndarray`
+                Explicit *y*-coords for moment arm
+            *z*: {``None``} | :class:`np.ndarray`
+                Explicit *z*-coords for moment arm
+            *mask*: :class:`np.ndarray`\ [:class:`bool` | :class:`int`]
+                Mask or indices of which cases to integrate 
+            *x*: {``None``} | :class:`np.ndarray`
+                Optional 1D or 2D *x*-coordinates directly specified
+            *xMRP*: {``DB.xMRP``} | :class:`float`
+                Moment reference point *x*-coordinate
+            *Lref*: {``DB.Lref``} | :class:`float`
+                Reference length for scaling moment (use ``1.0`` for
+                dimensional moment loads)
+        :Outputs:
+            *v*: :class:`np.ndarray`
+                2D array of moments derived from *db[col]*
+        :Versions:
+            * 2020-06-04 ``@ddalle``: First version
+        """
+        # Get mask
+        mask = kw.get("mask")
+        # Get dimension of *col*
+        ndim = self.get_col_prop(col, "Dimension")
+        # Ensure 2D column
+        if ndim != 2:
+            raise ValueError("Col '%s' is not 2D" % col)
+        # Get values
+        v = self.get_values(col, mask)
+        # Multiply
+        return self._genr8_ll_moment(v, ax1, ax2, **kw)
+
+    # Calculate moments
+    def _genr8_ll_moment(self, v, ax1, ax2, **kw):
+        r"""Create a moment line load based on a force line load
+
+        :Call:
+            >>> v = db._genr8_ll_moment(v, ax1, ax2, **kw)
+        :Inputs:
+            *db*: :class:`cape.attdb.rdb.DataKit`
+                Database with analysis tools
+            *v*: :class:`np.ndarray`
+                2D array of data to calculate moment of
             *ax1*: ``0`` | ``1`` | ``2`` | ``"x"`` | ``"y"`` | ``"z"``
                 Force axis direction
             *ax2*: ``0`` | ``1`` | ``2`` | ``"x"`` | ``"y"`` | ``"z"``
@@ -502,18 +557,17 @@ class DBLL(dbfm.DBFM):
         # Check *ax2*
         if ax2 not in {0, 1, 2, "x", "y", "z"}:
             raise ValueError("Unrecognized value for moment axis (*ax2*)")
-       # --- Slice Coordinates ---
+       # --- Values ---
+        # Check dimension
+        if not isinstance(v, np.ndarray):
+            raise TypeError("Input loads must be array")
+        elif v.ndim != 2:
+            raise IndexError("Input loads are not 2D")
+        # Number of conditions
+        ncut, nv = v.shape
+       # --- Cut Coordinates ---
         # Get mask
         mask = kw.get("mask")
-        # Get dimension of *col*
-        ndim = self.get_col_prop(col, "Dimension")
-        # Ensure 2D column
-        if ndim != 2:
-            raise ValueError("Col '%s' is not 2D" % col)
-        # Get values
-        V = self.get_values(col, mask)
-        # Number of conditions
-        ncut, nv = V.shape
         # Process *x*-coordinates
         xcol = kw.get("xcol")
         ycol = kw.get("ycol")
@@ -631,18 +685,18 @@ class DBLL(dbfm.DBFM):
                 # Tile if 1D
                 if ndimy == 1:
                     r = np.tile(r, (nv, 1)).T
-            elif ax2 in ["y", 0]:
+            elif ax2 in ["y", 1]:
                 # Calculate *dCLM* (pitching moment) from *dCN*
                 r = xmrp - x
                 # Tile if 1D
                 if ndimx == 1:
                     r = np.tile(r, (nv, 1)).T
-            elif ax2 in ["z", 0]:
+            elif ax2 in ["z", 2]:
                 # No possibility of *dCLN*) (yawing moment) from *dCN*
                 r = 0.0
        # --- Output ---
         # Multiply moment arm times load
-        return r * V
+        return r * v
 
     # Calculate pitching moment
     def genr8_dclm(self, col, xcol=None, **kw):
@@ -729,9 +783,9 @@ class DBLL(dbfm.DBFM):
   # <
    # --- Basis ---
     # Create basis for line load of one component
-    def create_ll3_basis(self, cols, nPOD=10, mask=None, **kw):
+    def genr8_ll3x_basis(self, cols, nPOD=10, mask=None, **kw):
         # Check types
-        if not isinstnace(cols, (list, tuple)):
+        if not isinstance(cols, (list, tuple)):
             # Wrong type
             raise TypeError("Adjusted col list must be list")
         elif len(cols) != 3:
@@ -745,16 +799,133 @@ class DBLL(dbfm.DBFM):
                 raise TypeError("Adjust col %i is not a 'str'" % j)
             elif col not in self:
                 raise KeyError("Adjust col '%s' not in database" % col)
+        # Other options
+        method = kw.get("method", "trapz")
+        # Get *xcol*
+        xcol = kw.get("xcol")
+        # Default *xcol*
+        if (xcol is None):
+            # Try to get the output *xarg*
+            xargs = self.get_output_xargs(col)
+            # Check if list
+            if isinstance(xargs, (list, tuple)) and len(xargs) > 0:
+                # Default
+                xcol = xargs[0]
+        # Confirm *xcol*
+        if not typeutils.isstr(xcol):
+            raise TypeError("Did not find a column for *x*-coords")
+        # Get values
+        x = self.get_all_values(xcol)
+        # Check if present
+        if x is None:
+            # No coordinates found
+            raise KeyError("No x-coords from col '%s'" % xcol)
+        elif not isinstance(x, np.ndarray):
+            # Bad type
+            raise TypeError("X-coord col '%s' is not an array" % xcol)
+        # Get dimension of *x*-coords
+        ndimx = x.ndim
+        # Confirm
+        if ndimx == 0:
+            # Cannot use scalar *x* here
+            raise IndexError("Cannot use scalar *x* coords")
+        elif ndimx == 2:
+            # Apply *mask*
+            x = self.get_values(xcol, mask)
+        elif ndimx > 2:
+            # Cannot use ND *x*
+            raise IndexError("Cannot use %i-D *x* coords" % ndimx)
         # Unpack
         col1, col2, col3 = cols
         # Initialize output
         basis = {}
         # Get *CA* loads
         dCA = self.get_values(col1, mask)
+        dCY = self.get_values(col2, mask)
+        dCN = self.get_values(col3, mask)
         # Dimensions
         nx, ny = dCA.shape
         # Calculate SVD
         UCA, sCA, VCA = svd(dCA)
+        UCY, sCY, VCY = svd(dCY)
+        UCN, sCN, VCN = svd(dCN)
+        # Downselect
+        if nPOD is not None:
+            # Select first *nPOD* mode shapes
+            UCA = UCA[:, :nPOD]
+            UCY = UCY[:, :nPOD]
+            UCN = UCN[:, :nPOD]
+            # Select the first *nPOD* singular values
+            sCA = sCA[:nPOD]
+            sCY = sCY[:nPOD]
+            sCN = sCN[:nPOD]
+        # Number of modes
+        nmode = sCA.size
+        # Calculate *L2* norm of each basis vector
+        L2CA = np.sqrt(np.sum(UCA**2, axis=0))
+        L2CY = np.sqrt(np.sum(UCY**2, axis=0))
+        L2CN = np.sqrt(np.sum(UCN**2, axis=0))
+        # Normalize basis functions
+        psiCA = UCA / np.tile(L2CA, (nx, 1))
+        psiCY = UCY / np.tile(L2CY, (nx, 1))
+        psiCN = UCN / np.tile(L2CN, (nx, 1))
+        # Max loads on any cut
+        mxCA = np.max(np.abs(psiCA), axis=0)
+        mxCY = np.max(np.abs(psiCY), axis=0)
+        mxCN = np.max(np.abs(psiCN), axis=0)
+        # Weights
+        wCA = mxCA / sCA
+        wCY = mxCY / sCY
+        wCN = mxCN / sCN
+        # Moments from normalized mode shapes
+        psiCLM = self._genr8_ll_moment(psiCN, 2, 1, x=x)
+        psiCLN = self._genr8_ll_moment(psiCY, 1, 2, x=x)
+        # Integrate normalized mode shapes
+        CAF = self._genr8_integral(psiCA, x, method)
+        CYF = self._genr8_integral(psiCY, x, method)
+        CNF = self._genr8_integral(psiCN, x, method)
+        CLMF = self._genr8_integral(psiCLM, x, method)
+        CLNF = self._genr8_integral(psiCLN, x, method)
+        # Form matrix for linear system of constraints
+        dCA = np.array([CAF])
+        dCY = np.array([CYF, CLNF])
+        dCN = np.array([CNF, CLMF])
+        # First two equations: equality constraints on *CN* and *CLM*
+        A1CA = np.hstack((dCA, np.zeros((1, 1))))
+        A1CY = np.hstack((dCY, np.zeros((2, 2))))
+        A1CN = np.hstack((dCN, np.zeros((2, 2))))
+        # Last *n* equations: derivatives of the Lagrangian
+        A2CA = np.hstack((np.diag(2*wCA), -dCA.T))
+        A2CY = np.hstack((np.diag(2*wCY), -dCY.T))
+        A2CN = np.hstack((np.diag(2*wCN), -dCN.T))
+        # Assemble matrices
+        ACA = np.vstack((A1CA, A2CA))
+        ACY = np.vstack((A1CY, A2CY))
+        ACN = np.vstack((A1CN, A2CN))
+        # Right-hand sides of equations
+        bCA = np.hstack(([1.0], np.zeros(nmode)))
+        bCF = np.hstack(([1.0, 0.0], np.zeros(nmode)))
+        bCM = np.hstack(([1.0, 0.0], np.zeros(nmode)))
+        # Solve linear systems
+        xCA = np.linalg.solve(ACA, bCA)
+        xCY = np.linalg.solve(ACY, bCF)
+        xCN = np.linalg.solve(ACN, bCF)
+        xCm = np.linalg.solve(ACN, bCM)
+        xCn = np.linalg.solve(ACY, bCM)
+        # Calculate linear combination of SVD modes
+        phiCA = np.dot(UCA, xCA[:nmode])
+        phiCY = np.dot(UCY, xCY[:nmode])
+        phiCN = np.dot(UCN, xCN[:nmode])
+        phiCm = np.dot(UCN, xCm[:nmode])
+        phiCn = np.dot(UCY, xCn[:nmode])
+        # Basis
+        return {
+            "dCA_CA": phiCA,
+            "dCY_CY": phiCY,
+            "dCN_CN": phiCN,
+            "dCN_CLM": phiCm,
+            "dCY_CLN": phiCn,
+        }
         
   # >
 
