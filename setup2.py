1a62ed94118a6915788bce772bf04c64397dd265
Derek Dalle <derek.j.dalle@nasa.gov>
2021-02-09 14:00:25 -0800

    Start process of moving to 'wheel' builds

--- setup2.py
+++ setup2.py
@@ -5,16 +5,19 @@
 import os
 import json
 import ConfigParser
-import distutils.core
+
+# Standard library partial imports
+from setuptools import Extension, setup
 
 
 # Path to this file
-fpwd = os.path.dirname(os.path.realpath(__file__))
+fdir = os.path.dirname(os.path.realpath(__file__))
+fcape = os.path.join(fdir, "cape")
 
 # Get a get/set type object
 config = ConfigParser.SafeConfigParser()
 # Read the configuration options
-config.read(os.path.join(fpwd, "config2.cfg"))
+config.read(os.path.join(fcape, "config2.cfg"))
 
 # C compiler flags
 cflags = config.get("compiler", "extra_cflags").split()
@@ -26,7 +29,7 @@ ldflags = config.get("compiler", "extra_ldflags").split()
 include_dirs = config.get("compiler", "extra_include_dirs").split()
 
 # Extensions JSON file
-extjson = os.path.join(fpwd, "extensions.json")
+extjson = os.path.join(fcape, "extensions.json")
 # Read extension settings
 extopts = json.load(open(extjson))
 
@@ -35,9 +38,11 @@ exts = []
 # Loop through specified extensions
 for (ext, opts) in extopts.items():
     # Get sources (ensure not :class:`unicode` objects)
-    extsources = [str(src) for src in opts["sources"]]
+    extsources = [
+        str(src) for src in opts["sources"]
+    ]
     # Create extension
-    _ext = distutils.core.Extension(
+    _ext = Extension(
         str(ext) + "2",
         include_dirs = include_dirs,
         extra_compile_args = cflags,
@@ -47,10 +52,11 @@ for (ext, opts) in extopts.items():
     exts.append(_ext)
 
 # Compile and link
-distutils.core.setup(
+setup(
     name="cape",
-    packages=["cape"],
-    package_dir={"cape": "."},
-    version="1.0",
+    version="1.0a1",
+    packages=[
+        "cape"
+    ],
     description="CAPE computational aerosciences package",
     ext_modules=exts)
