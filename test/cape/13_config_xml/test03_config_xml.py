f42487092dcc5b897fb206b7123433ceb78a8ea9
Aaron Christopher Burkhead <aaron.c.burkhead@nasa.gov>
2021-03-19 13:08:57 -0700

    Changed test03 output to STDOUT, can't force a FAIL though

--- test/cape/13_config_xml/test03_config_xml.py
+++ test/cape/13_config_xml/test03_config_xml.py
@@ -10,3 +10,8 @@ cfgj = cape.config.ConfigJSON("arrow.json")
 # Write arrow2 XML config from JSON
 cfgj.WriteXML("arrow2.xml", Name="bullet sample", Source="bullet.tri")
 
+# Open arrow2 XML
+with open("arrow2.xml", "r") as f:
+    # Print lines of f
+    for line in f:
+        print(line)
