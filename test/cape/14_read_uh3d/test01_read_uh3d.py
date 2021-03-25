1c4e3c7b3ce7576dac4191ac9b9043a7570aee35
Aaron Christopher Burkhead <aaron.c.burkhead@nasa.gov>
2021-03-25 15:32:06 -0700

    Complete test01_read_uh3d

--- test/cape/14_read_uh3d/test01_read_uh3d.py
+++ test/cape/14_read_uh3d/test01_read_uh3d.py
@@ -31,15 +31,15 @@ tri.WriteConfigXML(XMLFILE)
 
 # Map the AFLR3 boudnary conditions
 print("Mapping AFLR3 boundary conditions")
-#tri.MapBCs_ConfigAFLR3()
+tri.MapBCs_ConfigAFLR3()
 
 # Write the AFLR3 boundary condition summary
 print("Writing AFLR3 boundary conditions summary")
-#tri.config.WriteAFLR3BC(OUTPUT_PREFIX + ".bc")
+tri.config.WriteAFLR3BC(OUTPUT_PREFIX + ".bc")
 
 # Map the FUN3D boundary conditions
 print("Mapping FUN3D boundary conditions")
-#tri.config.WriteFun3DMapBC(OUTPUT_PREFIX + ".mapbc")
+tri.config.WriteFun3DMapBC(OUTPUT_PREFIX + ".mapbc")
 
 # Write surface
 print("Writing surface TRI file")
