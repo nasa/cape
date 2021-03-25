8e5a950f87bbd2229c351c21e094a2e1b8d2a1c9
Jamie Meeroff <jamie.g.meeroff@nasa.gov>
2021-03-25 12:23:38 -0700

    Add first attempt at auto Tecplot field maps
    Applied with the FieldMaps key on .json file
    If it is a list as before, that list will be used as field map
    If it is a dict:
        set key auto --> true will enable automatic field map detection
        set key slice to number of slices needed
        set manual key to user made list if wanted, only if auto ~= true
    
    Might need some more error checking

--- cape/cfdx/report.py
+++ cape/cfdx/report.py
@@ -76,6 +76,7 @@ from .. import tar
 # Paraview/Tecplot interfaces
 from .bin import pvpython
 from cape.filecntl.tecplot import ExportLayout, Tecscript
+import cape.plt as plt
 
 # Class to interface with report generation and updating.
 class Report(object):
@@ -4587,9 +4588,39 @@ class Report(object):
             # Read the Mach number option
             omach = opts.get_SubfigOpt(sfig, "Mach")
             # Read the zone numbers for each FIELDMAP command
-            grps = opts.get_SubfigOpt(sfig, "FieldMap")
+            grps = opts.get_SubfigOpt(sfig, "FieldMap") 
             # Read the Tecplot layout
             tec = self.ReadTecscript(fsrc)
+            # Parse fieldmaps
+            # If its a list, just use those values
+            if type(grps) == list:
+                fieldmaps = grps
+            elif type(grps) == dict:    
+                if grps.get('auto'):
+                    # Init fieldmap list
+                    fieldmaps = []
+                    # First read the first line of the layout to get plt name
+                    fplt =  tec.ReadKey(1)[1].strip("'\'\"")
+                    # Now we have to read the plt file to get field map
+                    tecplt = plt.Plt(fplt)
+                    # Append last zone to 
+                    fieldmaps.append(tecplt.nZone)
+                    # Parse slices, just adds the n+1 zone
+                    if grps.get('slice') > 0:
+                        for i in range(grps.get('slice')):
+                            fieldmaps.append(fieldmaps[i]+1)
+                elif grps.get('manual'):
+                    fieldmaps = grps.get('manual')
+            # If nothing, assume auto with no slices
+            else:
+                # Init fieldmap list
+                fieldmaps = []
+                # First read the first line of the layout to get plt name
+                fplt =  tec.ReadKey(1)[1].strip("'\'\"")
+                # Now we have to read the plt file to get field map
+                tecplt = plt.Plt(fplt)
+                # Append last zone to fieldmap list
+                fieldmaps.append(tecplt.nZone)
             # Set the Mach number if appropriate
             if omach:
                 try:
@@ -4597,11 +4628,11 @@ class Report(object):
                 except Exception:
                     pass
             # Set the field map
-            if grps is not None:
+            if fieldmaps is not None:
                 try:
-                    tec.SetFieldMap(grps)
+                    tec.SetFieldMap(fieldmaps)
                 except Exception:
-                    print("  Warning: FieldMap update '%s' failed" % grps)
+                    print("  Warning: FieldMap update '%s' failed" % fieldmaps)
                     pass
             # Layout
             self.PrepTecplotLayoutVars(tec, sfig, i)
