3f63ca0825470d543267290b972ebcadf4d36b36
Derek Dalle <derek.j.dalle@nasa.gov>
2021-06-26 22:28:24 -0700

    Add more progress on DataKitLoader

--- cape/attdb/datakitloader.py
+++ cape/attdb/datakitloader.py
@@ -12,9 +12,66 @@ DataKit parameters.
 
 # Standard library
 import os
+import re
 
 # Local modules
 from .rdb import DataKit
+
+
+# Default options
+rc = {
+    "DATAKIT_CLS": DataKit,
+    "DB_DIR": "db",
+    "DB_DIRS_BY_TYPE": {},
+    "DB_NAME_FORMATS": ["datakit"],
+    "DB_NAME": None,
+    "MODULE_NAME_REGEX": [".+"],
+    "MODULE_NAME_REGEX_GROUPS": = {},
+    "RAWDATA_DIR": "rawdata",
+}
+
+
+# Get default value from *rc*
+def getrc(kw, key, mode=0):
+    r"""Get option from *kw*, using default value from *rc*
+
+    :Call:
+        >>> v = getrc(kw, key, mode=0)
+    :Inputs:
+        *kw*: :class:`dict`
+            Keyword arguments from some function
+        *key*: :class:`str`
+            Name of parameter to inspect
+        *mode*: {``0``} | ``1``
+            If ``0``, ignore ``kw[key]`` if it is ``None``
+    :Outputs:
+        *v*: **any**
+            Either *kw[key]* or *rc[key]*
+    :Versions:
+        * 2021-06-26 ``@ddalle``: Version 1.0
+    """
+    # Check mode
+    if mode == 0:
+        # Get value from *kw*
+        v1 = kw.get(key)
+        # Don't use ``None`` from *kw*
+        if v1 is not None:
+            # Use it
+            return v1
+    elif mode == 1:
+        # Use value from *kw* even if ``None``
+        if key in kw:
+            return kw[key]
+    else:
+        # Invalid mode
+        raise ValueError("Invalid mode '%s'; must be 0 or 1" % mode)
+    # Check for option
+    if key in rc:
+        # Return default
+        return rc[key]
+    else:
+        # No default value
+        raise KeyError("No default value for key '%s'" % key)
 
 
 # Create class
@@ -34,7 +91,6 @@ class DataKitLoader(object):
     :Versions:
         * 2021-06-25 ``@ddalle``: Version 0.1; Started
     """
-    # Initialization method
     def __init__(self, name, fname, **kw):
         r"""Initialization method
 
@@ -47,19 +103,54 @@ class DataKitLoader(object):
         # Containing folder
         self.MODULE_DIR = os.path.dirname(self.MODULE_FILE)
         # Raw data folder
-        self.RAWDATA_DIR = kw.get("RAWDATA_DIR", "rawdata")
+        self.RAWDATA_DIR = getrc(kw, "RAWDATA_DIR")
         # Processed data folder
-        self.DB_DIR = kw.get("DB_DIR", "db")
-        self.DB_DIRS_BY_TYPE = {}
+        self.DB_DIR = getrc(kw, "DB_DIR")
+        self.DB_DIRS_BY_TYPE = getrc(kw, "DB_DIRS_BY_TYPE")
         # Datakit class (default)
-        self.DATAKIT_CLS = kw.get("DATAKIT_CLS", DataKit)
+        self.DATAKIT_CLS = getrc(kw, "DATAKIT_CLS")
+        # Regular expression items
+        self.MODULE_NAME_REGEX = getrc(kw, "MODULE_NAME_REGEX")
+        self.MODULE_NAME_REGEX_GROUPS = getrc(kw, "MODULE_NAME_REGEX_GROUPS")
+        #
 
-  # ===========
-  # RAW DATA
-  # ===========
-  # <
-    # Read a given file from raw data folder
-    def read_rawdata(self, fname, cls=None):
+    def _genr8_modname_regexes(self):
+        # Get the regular expressions for each "group"
+        grps = dict(self.MODULE_NAME_REGEX_GROUPS)
+        # Add full formatting for regular expression group
+        grps_re = {
+            k: "(?P<%s>%s)" % (k, v)
+            for k, v in grps.items()
+        }
+        # Get regular expression list
+        regex_list = self.MODULE_NAME_REGEX
+        # Check if it's a list
+        if not isinstance(regex_list, (list, tuple)):
+            # Create a singleton
+            regex_list = [regex_list]
+        # Output
+        return grps
+        
+    def read_rawdata(self, fname, ftype=None, cls=None, **kw):
+        r"""Read a file from the RAW_DATA folder
+
+        :Call:
+            >>> db = dkl.read_rawdata(fname, ftype=None, cls=None, **kw)
+        :Inputs:
+            *fname*: :class:`str`
+                Name of file to read from raw data folder
+            *ftype*: {``None``} | :class:`str`
+                Optional specifier to predetermine file type
+            *cls*: {``None``} | :class:`type`
+                Class to read *fname* other than *dkl.DATAKIT_CLS*
+            *kw*: :class:`dict`
+                Additional keyword arguments passed to *cls*
+        :Outputs:
+            *db*: *dkl.DATAKIT_CLS* | *cls*
+                DataKit instance read from *fname*
+        :Versions:
+            * 2021-06-25 ``@ddalle``: Version 1.0
+        """
         # Get aw data folder
         fdir = os.path.join(self.MODULE_DIR, self.RAWDATA_DIR)
         # File name
@@ -67,6 +158,13 @@ class DataKitLoader(object):
         # Default class
         if cls is None:
             cls = self.DATAKIT_CLS
-        # Read the file
-        return cls(fabs)
+        # Check for user-specified file type
+        if ftype is None:
+            # Let *cls* determine the file type
+            return cls(fabs, **kw)
+        else:
+            # Set additional keyword arg
+            kw[ftype] = fabs
+            # Read the file using *ftype* kwarg
+            return cls(**kw)
 
