74189b13d276ea8af6fac86603201d17355962bc
Derek Dalle <derek.j.dalle@nasa.gov>
2021-08-20 14:34:44 -0700

    Add 'dkit' script and 'python3 -m cape.attdb CMD [OPTIONS]'
    which for now only supports 'dkit writedb', but will be expanded

--- cape/attdb/cli.py
+++ cape/attdb/cli.py
@@ -10,3 +10,59 @@ r"""
 # Standard library modules
 import sys
 
+# Local modules
+from . import writedb
+from .. import argread
+from .. import text as textutils
+
+
+# Help message
+HELP_DKIT = r"""
+-------------------------------------------------
+``dkit``: Command-Line Interface to datakit tools
+-------------------------------------------------
+
+"""
+
+
+# main function
+def main():
+    # Process arguments
+    a, kw = argread.readkeys(sys.argv)
+    # Check first arg
+    if len(a) == 0:
+        # No command
+        print(textutils.markdown(HELP_DKIT))
+        return
+    # Get first arg, the name of the command
+    cmdname = a.pop(0)
+    # Normalize
+    cmd = normalize_cmd(cmdname)
+    # Filter first arg
+    if cmd in {"writedb", "write-db"}:
+        # Call datakit writer
+        return writedb.write_dbs(*a, **kw)
+    else:
+        print(textutils.markdown(HELP_DKIT))
+        print("")
+        print("Unrecognized command '%s'" % cmdname)
+
+
+# Normalize command names
+def normalize_cmd(cmdname):
+    r"""Normalize command names
+
+    :Call:
+        >>> cmd = normalize_cmd(cndname)
+    :Inputs:
+        *cmdname*: :class:`str`
+            Name of ``dkit`` command
+    :Outputs:
+        *cmd*: :class:`str`
+            Lower-case *cmdname* with ``_`` replaced with ``-``
+    :Versions:
+        * 2021-08-20 ``@ddalle``: Version 1.0
+    """
+    # Output
+    return cmdname.lower().replace("_", "-")
+
