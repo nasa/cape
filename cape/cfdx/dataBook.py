1a73c186d77aac4e15662afc346ffae5f4e8ddc4
Derek Dalle <derek.j.dalle@nasa.gov>
2022-03-07 15:44:26 -0800

    Add ShiftMRP fixes from cape.cfdx.report to cape.cfdx.dataBook

--- cape/cfdx/dataBook.py
+++ cape/cfdx/dataBook.py
@@ -904,26 +904,30 @@ class DataBook(dict):
             # Get type
             ttyp = topts.get("Type")
             # Only apply to "ShiftMRP"
-            if ttyp != "ShiftMRP":
-                continue
-            # Ensure points are calculated
-            self.cntl.PreparePoints(i)
-            # Component to use for current MRP
-            compID = self.cntl.opts.get_DataBookCompID(comp)
-            if isinstance(compID, list):
-                compID = compID[0]
-            # Get current MRP and Lref
-            x0 = self.cntl.opts.get_RefPoint(comp)
-            Lref = self.cntl.opts.get_RefLength(comp)
-            # Set those as defaults in transformation
-            x0 = topts.setdefault("FromMRP", x0)
-            x1 = topts.setdefault("ToMRP", x0)
-            topts.setdefault("RefLength", Lref)
-            # Expand if *x0* is a string
-            topts["FromMRP"] = self.cntl.opts.expand_Point(x0)
-            topts["ToMRP"] = self.cntl.opts.expand_Point(x1)
-        # Loop through the transformations.
-        for topts in tcomp:
+            if ttyp == "ShiftMRP":
+                # Use a copy to avoid changing cntl.opts
+                topts = dict(topts)
+                # Component to use for current MRP
+                compID = self.cntl.opts.get_DataBookCompID(comp)
+                if isinstance(compID, list):
+                    compID = compID[0]
+                # Reset points for default *FromMRP*
+                self.cntl.opts.reset_Points()
+                # Use MRP prior to transfformations as default *FromMRP*
+                x0 = self.cntl.opts.get_RefPoint(comp)
+                # Ensure points are calculated
+                self.cntl.PreparePoints(i)
+                # Use post-transformation MRP as default *ToMRP*
+                x1 = self.cntl.opts.get_RefPoint(comp)
+                # Get current Lref
+                Lref = self.cntl.opts.get_RefLength(comp)
+                # Set those as defaults in transformation
+                x0 = topts.setdefault("FromMRP", x0)
+                x1 = topts.setdefault("ToMRP", x1)
+                topts.setdefault("RefLength", Lref)
+                # Expand if *x0* is a string
+                topts["FromMRP"] = self.cntl.opts.expand_Point(x0)
+                topts["ToMRP"] = self.cntl.opts.expand_Point(x1)
             # Apply the transformation.
             FM.TransformFM(topts, self.x, i)
             
