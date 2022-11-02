662f301984001c97266bc90e102081464b2ea5a6
Derek J. Dalle <derek.j.dalle@nasa.gov>
2022-11-02 13:11:00 -0700

    Apply improvements to triangle/line to pt from v1.0 branch

--- cape/geom.py
+++ cape/geom.py
@@ -17,7 +17,7 @@ The main categories are:
 
 # Third-party modules
 import numpy as np
- 
+
 
 # Function to rotate a triangulation about an arbitrary vector
 def RotatePoints(X, v1, v2, theta):
@@ -351,21 +351,18 @@ def tris_have_pt(X, Y, x, y, **kw):
             Whether or not each tri contains the test point
     :Versions:
         * 2017-02-06 ``@ddalle``: Version 1.0
+        * 2022-11-02 ``@ddalle``: Version 2.0; use tri areas
     """
-    # Get types
-    tX = type(X).__name__
-    tY = type(Y).__name__
     # Check input type
-    if tX == "list":
-        # Convert to array
+    if isinstance(X, list):
         X = np.array(X)
-    elif tX != "ndarray":
+    elif not isinstance(X, np.ndarray):
         # Convert to array
         raise TypeError("Triangles must be arrays")
-    if tY == "list":
+    if isinstance(Y, list):
         # Convert to array
         Y = np.array(Y)
-    elif tY != "ndarray":
+    elif not isinstance(Y, np.ndarray):
         # Convert to array
         raise TypeError("Triangles must be arrays")
     # Test for single triangle
@@ -376,49 +373,82 @@ def tris_have_pt(X, Y, x, y, **kw):
     # Check dimensions
     if X.shape[1] != 3:
         # Not triangles
-        raise IndexError("Triangle arrays must have three columns")
+        raise IndexError("Triangle arrays must have at least two columns")
     elif Y.shape[1] != 3:
         # Not triangles (Y)
-        raise IndexError("Triangle arrays must have three columns")
+        raise IndexError("Triangle arrays must have at least two columns")
     elif X.shape[0] != Y.shape[0]:
         # Not matching
         raise ValueError(
             "X and Y coordinate arrays have different number of triangles")
-    # Check inputs
-    if type(x).__name__ == "ndarray":
-        # Repeat test point *x* coordinate
-        x = np.transpose(np.vstack((x,x,x)))
-    if type(y).__name__ == "ndarray":
-        # Repeat test point *y* coordinate
-        y = np.transpose(np.vstack((y,y,y)))
-    # Construct test point to the left of all triangles
-    x0 = np.min(X) - 1.0
-    y0 = y
-    # Construct test point below all the triangles
-    x1 = x
-    y1 = np.min(Y) - 1.0
-    # Construct test point above all the triangles
-    x2 = x
-    y2 = np.max(Y) + 1.0
-    # Construct list of segments that consists of each triangle edge
-    # Use inputs as start points; rotate vertices to get end points
-    X2 = np.transpose([X[:,1], X[:,2], X[:,0]])
-    Y2 = np.transpose([Y[:,1], Y[:,2], Y[:,0]])
-    # Draw three lines starting with (x,y)
-    #   Line 0: (x,y) to a point directly left of and outside tris
-    #   Line 1: (x,y) to a point directly below and outside tris
-    #   Line 2: (x,y) to a point directly above and outside tris
-    Q0 = edges_int_line(X, Y, X2, Y2, x, y, x0, y0)
-    Q1 = edges_int_line(X, Y, X2, Y2, x, y, x1, y1)
-    Q2 = edges_int_line(X, Y, X2, Y2, x, y, x2, y2)
-    # Count up intersections; each line must intersect odd # of edges
-    n0 = np.sum(Q0, axis=1)
-    n1 = np.sum(Q1, axis=1)
-    n2 = np.sum(Q2, axis=1)
-    # Check for odd number of intersections with all three test lines
-    Q = np.logical_and(n0%2==1, np.logical_and(n1%2==1, n2%2==1))
-    # Output
-    return Q
+    # Unpack
+    x0, x1, x2 = X.T
+    y0, y1, y2 = Y.T
+    # Store deltas
+    xx0 = x - x0
+    xx1 = x - x1
+    xx2 = x - x2
+    yy0 = y - y0
+    yy1 = y - y1
+    yy2 = y - y2
+    # Total area
+    A = np.abs((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0))
+    # Three component areas
+    A0 = np.abs(xx1*yy2 - xx2*yy1)
+    A1 = np.abs(xx2*yy0 - xx0*yy2)
+    A2 = np.abs(xx0*yy1 - xx1*yy0)
+    # Test sum of areas
+    return A0 + A1 + A2 <= A * (1 + 1e-6) + 2e-12
+
+
+# Get distance from point to a line segment
+def dist2_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw):
+    r"""Get square of distance from point(s) to line segment(s)
+
+    The test point can either be a single point or a collection of
+    points with dimensions equal to the collection of lines.
+
+    :Call:
+        >>> d2 = dist2_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw)
+    :Inputs:
+        *X1*: :class:`np.ndarray`\ [:class:`float`]
+            *x*-coords of start points of several line segments
+        *Y1*: :class:`np.ndarray`\ [:class:`float`]
+            *y*-coords of start points of several line segments
+        *X2*: :class:`np.ndarray`\ [:class:`float`]
+            *x*-coords of end points of several line segments
+        *Y2*: :class:`np.ndarray`\ [:class:`float`]
+            *y*-coords of end points of several line segments
+        *x*: :class:`float` | :class:`np.ndarray`
+            *x*-coord of test point(s)
+        *y*: :class:`float` | :class:`np.ndarray`
+            *y*-coord of test point(s)
+    :Outputs:
+        *d2*: :class:`np.ndarray`\ [:class:`float`]
+            Minimum distance from each segment to point
+    :Versions:
+        * 2022-11-01 ``@ddalle``: Version 1.0
+    """
+    # Deltas
+    x12 = X2 - X1
+    y12 = Y2 - Y1
+    x01 = X1 - x
+    y01 = Y1 - y
+    x02 = X2 - x
+    y02 = Y2 - y
+    # Square of length of segment(s)
+    L2 = np.fmax(1e-6, x12*x12 + y12*y12)
+    # Normal component of distance times length
+    Ld1 = x01*y12 - y01*x12
+    # "Behind" component of tangent distance
+    Ld2 = np.fmax(0, x01*x12 + y01*y12)
+    # "Beyond" component of tangent distance
+    Ld3 = np.fmax(0, -x02*x12 - y02*y12)
+    # Two components (only 0 or 1 of d2 and d3 is positive)
+    L2d2 = Ld1*Ld1 + (Ld2 + Ld3)**2
+    # Divide length of segment
+    return L2d2 / L2
+
 
 # Get distance from point to a line segment
 def dist_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw):
@@ -450,14 +480,6 @@ def dist_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw):
     """
     # Calculate lengths of each segment
     L = np.sqrt((X2-X1)**2 + (Y2-Y1)**2)
-    # Check test point types
-    tx = type(x).__name__
-    ty = type(y).__name__
-    # Check for scalar points
-    if tx not in ['ndarray', 'list']:
-        x = x*np.ones_like(X1)
-    if ty not in ['ndarray', 'list']:
-        y = y*np.ones_like(Y1)
     # Tangent/normal vector components
     TX = (X2 - X1) / L
     TY = (Y2 - Y1) / L
@@ -474,10 +496,70 @@ def dist_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw):
     # Check for segments where that nearest point is outside the segment
     I = np.logical_or(T < 0, T > L)
     # Distance from (x,y) to segment end points
-    W1 = np.sqrt((x[I]-X1[I])**2 + (y[I]-Y1[I])**2)
-    W2 = np.sqrt((x[I]-X2[I])**2 + (y[I]-Y2[I])**2)
+    if isinstance(x, np.ndarray):
+        W1 = (x[I]-X1[I])**2 + (y[I]-Y1[I])**2
+        W2 = (x[I]-X2[I])**2 + (y[I]-Y2[I])**2
+    else:
+        W1 = (x-X1[I])**2 + (y-Y1[I])**2
+        W2 = (x-X2[I])**2 + (y-Y2[I])**2
     # For such segments, use the distance to closest vertex
-    D[I] = np.fmin(W1, W2)
+    D[I] = np.sqrt(np.fmin(W1, W2))
+    # Output
+    return D
+
+
+# Get distance from a point
+def dist2_tris_to_pt(X, Y, x, y, **kw):
+    r"""Get square of distance from a point to a collection of triangles
+
+    Points that are inside the triangle return a distance of 0
+
+    :Call:
+        >>> D = dist_tris_to_pt(X, Y, x, y)
+    :Inputs:
+        *X*: :class:`np.ndarray`\ [:class:`float`]
+            *x*-coords of vertices of *n* tris, *shape*: (n,3)
+        *Y*: :class:`np.ndarray`\ [:class:`float`], shape=(n,3))
+            *y*-coords of vertices of *n* tris, *shape*: (n,3)
+        *x*: :class:`float`
+            *x*-coordinate of test point
+        *y*: :class:`float`
+            *y*-coordinate of test point
+    :Outputs:
+        *D*: :class:`np.ndarray`\ [:class:`float`]
+            Matrix of minimum distance squared from each tri to point
+    :Versions:
+        * 2017-02-06 ``@ddalle``: Version 1.0
+        * 2022-11-01 ``@ddalle``: Version 2.0; faster
+    """
+    # Check for membership of each triangle
+    Q = tris_have_pt(X, Y, x, y, **kw)
+    # Get the complementary list
+    Q0 = np.logical_not(Q)
+    # Number of triangles
+    n = Q.size
+    # Initialize distance
+    D = np.zeros(n)
+    # For tris that do not contain (x,y), get distance to each segment
+    X1 = X[Q0, :]
+    Y1 = Y[Q0, :]
+    X2 = X1[:, [1, 2, 0]] 
+    Y2 = Y1[:, [1, 2, 0]]
+    # Check for list of points
+    if isinstance(x, np.ndarray):
+        # Repeat for each vertex of the triangle
+        x = np.transpose(np.vstack((x, x, x)))
+        # Deselect points inside triangles
+        x = x[Q0, :]
+    if isinstance(y, np.ndarray):
+        # Repeat for each vertex of the triangle
+        y = np.transpose(np.vstack((y, y, y)))
+        # Deselect points inside triangles
+        y = y[Q0, :]
+    # Get dist to each segment of each tri that does not contain the pt
+    D2 = dist2_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw)
+    # Save the minimum pt-to-edge distance
+    D[Q0] = np.min(D2, axis=1)
     # Output
     return D
 
@@ -504,33 +586,7 @@ def dist_tris_to_pt(X, Y, x, y, **kw):
             Matrix of minimum distance from each tri to point
     :Versions:
         * 2017-02-06 ``@ddalle``: Version 1.0
+        * 2022-11-01 ``@ddalle``: Version 2.0; faster
     """
-    # Check for membership of each triangle
-    Q = tris_have_pt(X, Y, x, y, **kw)
-    # Get the complementary list
-    Q0 = np.logical_not(Q)
-    # Number of triangles
-    n = Q.size
-    # Initialize distance
-    D = np.zeros(n)
-    # For tris that do not contain (x,y), get distance to each segment
-    X1 = X[Q0,:]; X2 = X1[:,[1,2,0]]
-    Y1 = Y[Q0,:]; Y2 = Y1[:,[1,2,0]]
-    # Check for list of points
-    if type(x).__name__ == "ndarray":
-        # Repeat for each vertex of the triangle
-        x = np.transpose(np.vstack((x,x,x)))
-        # Deselect points inside triangles
-        x = x[Q0,:]
-    if type(y).__name__ == "ndarray":
-        # Repeat for each vertex of the triangle
-        y = np.transpose(np.vstack((y,y,y)))
-        # Deselect points inside triangles
-        y = y[Q0,:]
-    # Get dist to each segment of each tri that does not contain the pt
-    D0 = dist_lines_to_pt(X1, Y1, X2, Y2, x, y, **kw)
-    # Save the minimum pt-to-edge distance
-    D[Q0] = np.min(D0, axis=1)
-    # Output
-    return D
+    return np.sqrt(dist2_tris_to_pt(X, Y, x, y, **kw))
 
