1d9aba25a956724f7a1751e0978476151f555003
Derek J. Dalle <derek.j.dalle@nasa.gov>
2022-11-03 12:06:31 -0700

    Finish AdaptCartOpts

--- cape/pycart/options/runctlopts.py
+++ cape/pycart/options/runctlopts.py
@@ -31,13 +31,13 @@ but with several special methods, including command-line options to
 from .archiveopts import ArchiveOpts
 from .util import rc0, odict, isArray, getel
 from ...cfdx.options import runctlopts
-from ...cfdx.options.util import ExecOpts
+from ...cfdx.options.util import ExecOpts, OptionsDict
 from ...optdict import BOOL_TYPES, FLOAT_TYPES, INT_TYPES
 
 
 # Class for flowCart inputs
 class FlowCartOpts(ExecOpts):
-    """Class for ``flowCart`` settings
+    r"""Class for ``flowCart`` settings
 
     :Call:
         >>> opts = FlowCartOpts(**kw)
@@ -174,7 +174,7 @@ class FlowCartOpts(ExecOpts):
         "vizTD": "steps between ``flowCart`` visualization outputs",
         "y_is_spanwise": "whether *y* is spanwise axis for ``flowCart``",
     }
-    
+
 
 # Add properties
 FlowCartOpts.add_properties(FlowCartOpts._optlist)
@@ -182,125 +182,73 @@ FlowCartOpts.add_properties(FlowCartOpts._optlist)
 
 # Class for flowCart settings
 class AdjointCartOpts(ExecOpts):
-    """Dictionary-based interfaced for options specific to ``adjointCart``"""
-    # Number of iterations for adjointCart
-    def get_it_ad(self, i=None):
-        """Return the number of iterations for `adjointCart`
-        
-        :Call:
-            >>> it_fc = opts.get_it_fc(i=None)
-        :Inputs:
-            *opts*: :class:`cape.pycart.options.Options`
-                Options interface
-            *i*: :class:`int` or ``None``
-                Phase number
-        :Outputs:
-            *it_ad*: :class:`int` or :class:`list`(:class:`int`)
-                Number of iterations for run *i* or all runs if ``i==None``
-        :Versions:
-            * 2014.08.01 ``@ddalle``: Version 1.0
-        """
-        return self.get_key('it_ad', i)
-        
-    # Set adjointCart iteration count
-    def set_it_ad(self, it_ad=rc0('it_ad'), i=None):
-        """Set the number of iterations for `adjointCart`
-        
-        :Call:
-            >>> opts.set_it_ad(it_ad)
-            >>> opts.set_it_ad(it_ad, i)
-        :Inputs:
-            *opts*: :class:`cape.pycart.options.Options`
-                Options interface
-            *it_ad*: :class:`int` or :class:`list`(:class:`int`)
-                Number of iterations for run *i* or all runs if ``i==None``
-            *i*: :class:`int` or ``None``
-                Phase number
-        :Versions:
-            * 2014.08.01 ``@ddalle``: Version 1.0
-        """
-        self.set_key('it_ad', it_ad, i)
-        
-        
-    # Get adjointCart multigrd levels
-    def get_mg_ad(self, i=None):
-        """Return the number of multigrid levels for `adjointCart`
-        
-        :Call:
-            >>> mg_fc = opts.get_mg_ad(i=None)
-        :Inputs:
-            *opts*: :class:`cape.pycart.options.Options`
-                Options interface
-            *i*: :class:`int` or ``None``
-                Phase number
-        :Outputs:
-            *mg_fc*: :class:`int` or :class:`list`(:class:`int`)
-                Multigrid levels for run *i* or all runs if ``i==None``
-        :Versions:
-            * 2014.08.02 ``@ddalle``: Version 1.0
-        """
-        return self.get_key('mg_ad', i)
-    
-    # Set adjointCart iteration levels
-    def set_mg_ad(self, mg_ad=rc0('mg_ad'), i=None):
-        """Set number of multigrid levels for `adjointCart`
-        
-        :Call:
-            >>> opts.set_mg_ad(mg_ad, i)
-        :Inputs:
-            *opts*: :class:`cape.pycart.options.Options`
-                Options interface
-            *mg_ad*: :class:`int` or :class:`list`(:class:`int`)
-                Multigrid levels for run *i* or all runs if ``i==None``
-            *i*: :class:`int` or ``None``
-                Phase number
-        :Versions:
-            * 2014.08.02 ``@ddalle``: Version 1.0
-        """
-        self.set_key('mg_ad', mg_ad, i)
-        
-        
-    # Adjoint first order
-    def get_adj_first_order(self, i=None):
-        """Get whether or not to run adjoins in first-order mode
-        
-        :Call:
-            >>> adj = opts.set_adj_first_order(i)
-        :Inputs:
-            *opts*: :class:`cape.pycart.options.Options`
-                Options interface
-            *i*: :class:`int` or ``None``
-                Phase number
-        :Outputs:
-            *adj*: :class:`bool` or :class:`list`(:class:`int`)
-                Whether or not to always run `adjointCart` first-order
-        :Versions:
-            * 2014-11-17 ``@ddalle``: Version 1.0
-        """
-        return self.get_key('adj_first_order', i)
-        
-    # Adjoint first order
-    def set_adj_first_order(self, adj=rc0('adj_first_order'), i=None):
-        """Set whether or not to run adjoins in first-order mode
-        
-        :Call:
-            >>> opts.set_adj_first_order(adj, i)
-        :Inputs:
-            *opts*: :class:`cape.pycart.options.Options`
-                Options interface
-            *adj*: :class:`bool` or :class:`list`(:class:`int`)
-                Whether or not to always run `adjointCart` first-order
-            *i*: :class:`int` or ``None``
-                Phase number
-        :Versions:
-            * 2014-11-17 ``@ddalle``: Version 1.0
-        """
-        self.set_key('adj_first_order', adj, i)
+    r"""Class for ``adjointCart`` settings
+
+    :Call:
+        >>> opts = AdjointCartOpts(**kw)
+    :Inputs:
+        *kw*: :class:`dict`
+            Raw options
+    :Outputs:
+        *opts*: :class:`AdjointCartOpts`
+            CLI options for ``adjointCart``
+    :Versions:
+        * 2014-12-17 ``@ddalle``: Version 1.0 (``adjointCart``)
+        * 2022-11-02 ``@ddalle``: Version 2.0; use ``optdict``
+    """
+    __slots__ = ()
+
+    # Accepted options
+    _optlist = {
+        "adj_first_order",
+        "it_ad",
+        "mg_ad",
+    }
+
+    # Types
+    _opttypes = {
+        "adj_first_order": BOOL_TYPES,
+        "it_ad": INT_TYPES,
+        "mg_ad": INT_TYPES,
+    }
+
+    # Defaults
+    _rc = {
+        "adj_first_order": False,
+        "it_ad": 120,
+        "mg_ad": 3,
+    }
+
+    # Descriptions
+    _rst_descriptions = {
+        "whether to run ``adjointCart`` first-order",
+        "number of iterations for ``adjointCart``",
+        "multigrid levels for ``adjointCart``",
+    }
 
 
-# Class for flowCart settings
-class Adaptation(odict):
-    r"""Dictionary-based interfaced for options for Cart3D adaptation"""
+# Add properties
+AdjointCartOpts.add_properties(AdjointCartOpts._optlist)
+
+
+# Class for mesh adaptation settings
+class AdaptationOpts(OptionsDict):
+    r"""Class for Cart3D mesh adaptation settings
+
+    This mostly affects the file ``aero.csh``.
+
+    :Call:
+        >>> opts = AdaptationOpts(**kw)
+    :Inputs:
+        *kw*: :class:`dict`
+            Raw options
+    :Outputs:
+        *opts*: :class:`AdaptationOpts`
+            Cart3D mesh adaptation settings for pycart
+    :Versions:
+        * 2014-12-17 ``@ddalle``: Version 1.0 (``Adaptation``)
+        * 2022-11-02 ``@ddalle``: Version 2.0; use ``optdict``
+    """
     # Get number of adaptation cycles
     def get_n_adapt_cycles(self, i=None):
         """Return the number of Cart3D number of adaptation cycles
