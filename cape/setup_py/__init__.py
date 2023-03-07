82eebb6f03efbca9f12a2ef102ed9141fe3c2f59
Derek Dalle <derek.j.dalle@nasa.gov>
2023-03-07 11:36:24 -0800

    Move maing setup() instructions to internal module

---
+++ cape/setup_py/__init__.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Standard library
+import sys
+
+
+# Python version infor
+PY_MAJOR_VERSION = sys.version_info.major
+PY_MINOR_VERSION = sys.version_info.minor
+
+
+# Compile and link
+SETUP_SETTINGS = dict(
+    name="cape",
+    version="1.0.0rc3",
+    description="CAPE computational aerosciences package",
+    url="https://www.github.com/nasa/cape",
+    author_email="derek.j.dalle@nasa.gov",
+    license="NASA Open Source Agreement Version 1.3",
+    packages=[
+        "cape",
+        "cape.attdb",
+        "cape.attdb.ftypes",
+        "cape.cfdx",
+        "cape.cfdx.options",
+        "cape.filecntl",
+        "cape.pycart",
+        "cape.pycart.options",
+        "cape.pyfun",
+        "cape.pyfun.options",
+        "cape.pyover",
+        "cape.pyover.options",
+        "cape.tnakit",
+        "cape.tnakit.plot_mpl",
+        "cape.tnakit.textutils"
+    ],
+    install_requires=[
+        "defusedxml",
+        "numpy>=1.4.1",
+        "matplotlib>=2",
+        "scipy",
+        "vendorize",
+        "xlrd%i" % PY_MAJOR_VERSION,
+        "xlsxwriter"
+    ],
+    package_data={
+        "cape": [
+            "templates/paraview/*",
+            "templates/tecplot/*"
+        ],
+        "cape.cfdx": ["templates/*"],
+        "cape.cfdx.options": ["*.json"],
+        "cape.pycart": ["templates/*"],
+        "cape.pycart.options": ["*.json"],
+        "cape.pyfun": ["templates/*"],
+        "cape.pyfun.options": ["*.json"],
+        "cape.pyover": ["templates/*"],
+        "cape.pyover.options": ["*.json"],
+    },
+    scripts=[
+        "bin/run_flowCart.py",
+        "bin/run_fun3d.py",
+        "bin/run_overflow.py"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pycart=cape.pycart.cli:main",
+            "pyfun=cape.pyfun.cli:main",
+            "pyover=cape.pyover.cli:main",
+            "dkit=cape.attdb.cli:main",
+            "dkit-quickstart=cape.attdb.quickstart:main",
+            "dkit-vendorize=cape.attdb.vendorutils:main",
+            "dkit-writedb=cape.attdb.writedb:main",
+            "cape-writell=cape.writell:main",
+            "cape-step2crv=cape.tricli:main_step2crv",
+            "cape-steptri2crv=cape.tricli:main_steptri2crv",
+            "cape-uh3d2tri=cape.tricli:main_uh3d2tri",
+            "cape-tri2uh3d=cape.tricli:main_tri2uh3d",
+            "cape-tri2plt=cape.tricli:main_tri2plt",
+            "cape-tri2surf=cape.tricli:main_tri2surf",
+            "pyfun-plt2triq=cape.pyfun.tricli:main_plt2triq",
+        ],
+    })
