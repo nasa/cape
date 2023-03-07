c517e30e27db90b7b58c0e3386ad1b95110b66ca
Derek Dalle <derek.j.dalle@nasa.gov>
2023-03-06 16:13:52 -0800

    Work on consolidating setup.py and setup_with_extensions.py

--- cape/__init__.py
+++ cape/__init__.py
@@ -2,36 +2,31 @@
 r"""
 
 The :mod:`cape` module contains the top-level interface setup of various
-solvers. It loads the most important methods from the various submodules so that
-they are easier to access. Most tasks using the Cape API can be accessed by
-loading this module.
+solvers. It loads the most important methods from the various submodules
+so that they are easier to access. Most tasks using the Cape API can be
+accessed by loading this module.
 
     .. code-block:: python
     
         import cape
         
-For example the following will read in a global settings instance assuming that
-the present working directory contains the correct files.  (If not, various
-defaults will be used, but it is unlikely that the resulting setup will be what
-you intended.)
+For example the following will read in a global settings instance
+assuming that the present working directory contains the correct files.
+(If not, various defaults will be used, but it is unlikely that the
+resulting setup will be what you intended.)
 
     .. code-block:: python
         
         import cape
         cntl = cape.Cntl()
         
-Most of the pyCart submodules essentially contain a single class definition,
-and many of these classes are accessible directly from the :mod:`cape` module.
-The list of classes loaded directly in :mod:`cape`.
+Most of the pyCart submodules essentially contain a single class
+definition, and many of these classes are accessible directly from the
+:mod:`cape` module. The list of classes loaded directly in :mod:`cape`.
 
     * :class:`cape.cntl.Cntl`
     * :class:`cape.tri.Tri`
     * :class:`cape.tri.Triq`
-    
-Because Cape is a template module that has no specific solver, few modules are
-loaded directly to :mod:`cape`.  The list of modules loaded are shown below.
-
-    * :mod:`cape.manage`
 
 A categorized list of modules available to the API are listed below.
 
@@ -77,40 +72,37 @@ A categorized list of modules available to the API are listed below.
        - :mod:`cape.cfdx.volcomp`
     
 
-:Versions:
-    * Version 0.6: 2016-03-30
-    * Version 0.8: 2018-03-23
-    * Version 0.9.2: 2019-09-17
-    * Version 1.0: 2020-01-21
-
 **Notices**
 
-Copyright © 2022 United States Government as represented by the Administrator
-of the National Aeronautics and Space Administration.  All Rights Reserved.
+Copyright © 2022 United States Government as represented by the
+Administrator of the National Aeronautics and Space Administration.  All
+Rights Reserved.
 
 **Disclaimers**
 
-No Warranty: THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF
-ANY KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED
-TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY
-IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
-FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL BE ERROR
-FREE, OR ANY WARRANTY THAT DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE
+No Warranty: THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY
+WARRANTY OF ANY KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY,
+INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE
+WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR FREEDOM FROM
+INFRINGEMENT, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL BE ERROR FREE,
+OR ANY WARRANTY THAT DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE
 SUBJECT SOFTWARE. THIS AGREEMENT DOES NOT, IN ANY MANNER, CONSTITUTE AN
 ENDORSEMENT BY GOVERNMENT AGENCY OR ANY PRIOR RECIPIENT OF ANY RESULTS,
 RESULTING DESIGNS, HARDWARE, SOFTWARE PRODUCTS OR ANY OTHER APPLICATIONS
 RESULTING FROM USE OF THE SUBJECT SOFTWARE.  FURTHER, GOVERNMENT AGENCY
-DISCLAIMS ALL WARRANTIES AND LIABILITIES REGARDING THIRD-PARTY SOFTWARE, IF
-PRESENT IN THE ORIGINAL SOFTWARE, AND DISTRIBUTES IT "AS IS."
+DISCLAIMS ALL WARRANTIES AND LIABILITIES REGARDING THIRD-PARTY SOFTWARE,
+IF PRESENT IN THE ORIGINAL SOFTWARE, AND DISTRIBUTES IT "AS IS."
 
-Waiver and Indemnity:  RECIPIENT AGREES TO WAIVE ANY AND ALL CLAIMS AGAINST THE
-UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY
-PRIOR RECIPIENT.  IF RECIPIENT'S USE OF THE SUBJECT SOFTWARE RESULTS IN ANY
-LIABILITIES, DEMANDS, DAMAGES, EXPENSES OR LOSSES ARISING FROM SUCH USE,
-INCLUDING ANY DAMAGES FROM PRODUCTS BASED ON, OR RESULTING FROM, RECIPIENT'S
-USE OF THE SUBJECT SOFTWARE, RECIPIENT SHALL INDEMNIFY AND HOLD HARMLESS THE
-UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY
-PRIOR RECIPIENT, TO THE EXTENT PERMITTED BY LAW.  RECIPIENT'S SOLE REMEDY FOR
+Waiver and Indemnity:  RECIPIENT AGREES TO WAIVE ANY AND ALL CLAIMS
+AGAINST THE UNITED STATES GOVERNMENT, ITS CONTRACTORS AND
+SUBCONTRACTORS, AS WELL AS ANY PRIOR RECIPIENT.  IF RECIPIENT'S USE OF
+THE SUBJECT SOFTWARE RESULTS IN ANY LIABILITIES, DEMANDS, DAMAGES,
+EXPENSES OR LOSSES ARISING FROM SUCH USE, INCLUDING ANY DAMAGES FROM
+PRODUCTS BASED ON, OR RESULTING FROM, RECIPIENT'S USE OF THE SUBJECT
+SOFTWARE, RECIPIENT SHALL INDEMNIFY AND HOLD HARMLESS THE UNITED STATES
+GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY PRIOR
+RECIPIENT, TO THE EXTENT PERMITTED BY LAW.  RECIPIENT'S SOLE REMEDY FOR
 ANY SUCH MATTER SHALL BE THE IMMEDIATE, UNILATERAL TERMINATION OF THIS
 AGREEMENT.
 """
@@ -123,7 +115,7 @@ from .cntl import Cntl
 
 
 # Save version number
-version = "1.0rc4"
+version = "1.0.0rc4"
 __version__ = version
 
 # Get the root directory of the module.
