14bf6405deafae7ad49bd757a0787be4ce6e0503
Derek Dalle <derek.j.dalle@nasa.gov>
2023-03-20 15:55:08 -0700

    Test remaining pyfun.options modules

---
+++ test/007_pyfun/01_options/test_pyfunarchiveopts.py
@@ -0,0 +1,17 @@
+
+# Local
+from cape.pyfun.options.archiveopts import auto_Archive, ArchiveOpts
+
+
+# Template options
+OPTS1 = {
+    "ArchiveFormat": "tgz",
+    "PostDeleteFiles": "*.flow",
+}
+
+
+def test_ArchiveOpts():
+    # Full-folder archive
+    opts1 = auto_Archive(OPTS1)
+    assert isinstance(opts1, ArchiveOpts)
+
