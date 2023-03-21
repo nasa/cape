1557d80eeb87fbf4db17a92936ec77312143d287
Derek Dalle <derek.j.dalle@nasa.gov>
2023-03-20 22:59:14 -0700

    Continue initial conversions for pyus.options

--- cape/pyus/options/runctlopts.py
+++ cape/pyus/options/runctlopts.py
@@ -1,55 +1,27 @@
-"""
-:mod:`cape.pyus.options.runControl.RunControl`: Run control options
-=====================================================================
+r"""
 
-Options interface for aspects of running a case of US3D.  The settings are
-read from the ``"RunControl"`` of a JSON file, and the contents of this section
-are written to :file:`case.json` within each run folder.
+Options interface for aspects of running a case of US3D. The settings
+are read from the ``"RunControl"`` of a JSON file, and the contents of
+this section are written to ``case.json`` within each run folder.
 
-The methods of :class:`cape.cfdx.options.runControl.RunControl` are also present.
-These control options such as whether to submit as a PBS job, whether or not to
-use MPI, etc.
+The methods of :class:`cape.cfdx.options.runctlopts.RunControlOpts` are
+also present. These control options such as whether to submit as a PBS
+job, whether or not to use MPI, etc.
 
-This contains options that determine how long the solver is ran (primarily 
-via the ``"PhaseSequence"`` and ``"PhaseIters"`` options), what basic mode it
-is run in (such as a submitted or local job or serial or MPI job), and options
-for command-line options to the FUN3D binaries.  There is also an
-``"Archive"`` section that can be used for copying files and cleaning up after
-one or more cases have been completed.
-
-This module primarily provides a class :class:`pyFun.options.RunControl`. Many
-of the options that are common to all solvers are inherited from
-:class:`cape.cfdx.options.runControl.RunControl`. This class also has an interface
-for environment variables and ``ulimit`` parameters.
-
-In particular, all of the commands available to the classes listed below are
-also available to :class:`pyFun.options.runControl.RunControl`.
-
-:Classes:
-    * :class:`pyUS.options.runControl.RunControl`
-    * :class:`pyUS.options.runControl.nodet`
-    * :class:`pyUS.options.Archive.Archive`
-    * :class:`cape.cfdx.options.aflr3.aflr3`
-    * :class:`cape.cfdx.options.runControl.Environ`
-    * :class:`cape.cfdx.options.ulimit.ulimit`
 :See Also:
-    * :mod:`cape.cfdx.options.runControl`
-    * :mod:`cape.cfdx.options.ulimit`
-    * :mod:`cape.cfdx.options.intersect`
-    * :mod:`cape.pyus.options.Archive`
+    * :mod:`cape.cfdx.options.runctlopts`
+    * :mod:`cape.cfdx.options.ulimitopts`
+    * :mod:`cape.pyus.options.archiveopts`
 """
 
-# Import options-specific utilities
-from .util import rc0, getel, odict
+# Local imports
+from ...cfdx.options import runctlopts
+from ...cfdx.options.util import ExecOpts
+from .archiveopts import ArchiveOpts
 
-# Import template module
-import cape.cfdx.options.runControl
-
-# Submodules
-from .Archive import Archive
 
 # Class for inputs to the US3D executable
-class US3D(odict):
+class US3DRunOpts(ExecOpts):
     # Option for input file
     def get_us3d_input(self, i=None):
         r"""Get name of input file for US3D
@@ -163,7 +135,7 @@ class US3D(odict):
 
 
 # Class for inputs to the ``us3d-prepar`` executable
-class US3DPrepar(odict):
+class US3DPreparOpts(ExecOpts):
     # Option to run ``us3d-prepar``
     def get_us3d_prepar_run(self, i=0):
         r"""Get option to run or not run ``us3d-prepar``
@@ -314,87 +286,21 @@ class US3DPrepar(odict):
 
 
 # Class for Report settings
-class RunControl(cape.cfdx.options.runControl.RunControl):
-    """Dictionary-based interface for automated reports
-    
-    :Call:
-        >>> opts = RunControl(**kw)
-    :Versions:
-        * 2015-09-28 ``@ddalle``: Subclassed to CAPE
-    """
-    
-    # Initialization method
-    def __init__(self, fname=None, **kw):
-        # Store the data in *this* instance
-        for k in kw:
-            self[k] = kw[k]
-        # Upgrade important groups to their own classes.
-        self._Environ()
-        self._ulimit()
-        self._US3DPrepar()
-        self._Archive()
-        
-   # ============ 
-   # Initializers
-   # ============
-   # <
-    # Initialization and confirmation for ``us3d-prepar`` options
-    def _US3DPrepar(self):
-        """Initialize ``us3d-prepar`` options if necessary"""
-        # Get object
-        opts = self.get("us3d-prepar", {})
-        # Check types
-        if isinstance(opts, US3DPrepar):
-            # Nothing to do
-            pass
-        elif isinstance(opts, dict):
-            # Convert to special class
-            self["us3d-prepar"] = US3DPrepar(**opts)
-        elif opts:
-            # Create empty class with defaults
-            self["us3d-prepar"] = US3DPrepar(run=True)
-        else:
-            # Create empty class, ``us3d-prepar`` turned off
-            self["us3d-prepar"] = US3DPrepar(run=False)
+class RunControlOpts(runctlopts.RunControlOpts):
+    # No additional atributes
+    __slots__ = ()
 
-    # Initialization and confirmation for options to US3D executable
-    def _US3D(self):
-        """Initialize ``us3d`` options if necessary"""
-        # Get object
-        opts = self.get("us3d", {})
-        # Check types
-        if isinstance(opts, US3D):
-            # Nothing to do
-            pass
-        elif isinstance(opts, dict):
-            # Convert to special class
-            self["us3d"] = US3D(**opts)
-        else:
-            # Empty
-            self["us3d"] = US3D()
+    # Additional options
+    _optlist = {
+        "us3d-prepar",
+    }
 
-    # Initialization method for folder management options
-    def _Archive(self):
-        """Initialize folder management options if necessary"""
-        # Get object
-        opts = self.get("Archive", {})
-        # Check type
-        if isinstance(opts, Archive):
-            # Nothing to do
-            pass
-        elif isinstance(opts, dict):
-            # Convert
-            self["Archive"] = Archive(**opts)
-        else:
-            # Bad type
-            raise TypeError('"Archive" option has bad type')
-   # >
-   
-   # ============== 
-   # Local settings
-   # ==============
-   # <
-   # >
+    # Section map
+    _sec_cls = {
+        "Archive": ArchiveOpts,
+        "us3d": US3DRunOpts,
+        "us3d-prepar": US3DPreparOpts,
+    }
 
    # =============
    # us3d-prepar
@@ -440,14 +346,14 @@ class RunControl(cape.cfdx.options.runControl.RunControl):
         self._US3DPrepar()
         self["us3d-prepar"].set_us3d_prepar_output(fout, i=i)
 
-    # Copy documentation     
+    # Copy documentation
     for k in ["run", "grid", "conn", "output"]:
         n1 = "get_us3d_prepar_" + k
         n2 = "set_us3d_prepar_" + k
         eval(n1).__doc__ = getattr(US3DPrepar, n1).__doc__
         eval(n2).__doc__ = getattr(US3DPrepar, n2).__doc__
    # >
-    
+
    # =================
    # us3d
    # =================
@@ -482,7 +388,7 @@ class RunControl(cape.cfdx.options.runControl.RunControl):
         self._US3D()
         self["us3d"].set_us3d_gas(gas, i=i)
 
-    # Copy documentation     
+    # Copy documentation
     for k in ["input", "grid", "gas"]:
         n1 = "get_us3d_" + k
         n2 = "set_us3d_" + k
