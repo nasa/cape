5a310ba251c3c71afa575ddcf42820535922fc3c
Derek Dalle <derek.j.dalle@nasa.gov>
2023-05-16 13:32:02 -0700

    Start conversion of Fun3D 'Functional' section, but unsure
    Is this section really necessary?

--- cape/pyfun/options/functionalopts.py
+++ cape/pyfun/options/functionalopts.py
@@ -2,7 +2,171 @@
 
 
 # Local imports
-from ...optdict import OptionsDict
+from ...optdict import OptionsDict, FLOAT_TYPES
+
+
+# Class for individual function opts
+class FunctionOpts(OptionsDict):
+    # Attributes
+    __slots__ = ()
+
+    # Options
+    _optlist = {
+        "coeffs",
+        "parent",
+        "type",
+    }
+
+    # Aliases
+    _optmap = {
+        "cols": "coeffs",
+    }
+
+    # Types
+    _opttypes = {
+        "coeffs": str,
+        "parent": str,
+        "type": str,
+    }
+
+    # List depth
+    _optlistdepth = {
+        "coeffs": 1,
+    }
+
+    # Values
+    _optvals = {
+        "type": ("adapt", "constraint", "objective"),
+    }
+
+    # Defaults
+    _rc = {
+        "type": "adapt",
+    }
+
+
+# Collection of functions
+class FunctionCollectionOpts(OptionsDict):
+    # Attributes
+    __slots__ = ()
+
+    # Types
+    _opttypes = {
+        "_default_": dict,
+    }
+
+    # Sections
+    _sec_cls_opt = "Parent"
+    _sec_cls_optmap = {
+        "_default_": FunctionOpts,
+    }
+
+    # Get option for a function
+    def get_FunctionalFuncOpt(self, fn: str, opt: str, j=None, **kw):
+        r"""Get option for a specific functional function
+
+        :Call:
+            >>> v = opts.get_FunctionalFuncOpt(fn, opt, j=None, **kw)
+        :Inputs:
+            *opts*: :class:`OptionsDict`
+                Options interface
+            *fn*: :class:`str`
+                Name of function
+            *opt*: :class:`str`
+                Name of functional option
+        :Outputs:
+            *v*: :class:`object`
+                Value of ``opts[fn][opt]`` or as appropriate
+        :Versions:
+            * 2023-05-16 ``@ddalle``: v1.0
+        """
+        return self.get_subopt(fn, opt, j=j, **kw)
+
+    # Get list of optimization functions
+    def get_FunctionalAdaptFuncs(self):
+        r"""Get list of adaptation functions
+
+        :Call:
+            >>> funcs = opts.get_FunctionalAdaptFuncs()
+        :Inputs:
+            *opts*: :class:`OptionsDict`
+                Options interface
+        :Outputs:
+            *funcs*: :class:`list`\ [:class:`str`]
+                List of function names
+        :Versions:
+            * 2016-04-25 ``@ddalle``: v1.0
+            * 2023-05-16 ``@ddalle``: v2.0
+        """
+        # List of functions
+        funcs = []
+        # Loop through keys
+        for fn in self:
+            # Get type
+            typ = self.get_FunctionalFuncOpt(fn, "type")
+            # Check type
+            if typ == "adapt":
+                funcs.append(fn)
+        # Output
+        return funcs
+
+    # Get list of optimization functions
+    def get_FunctionalConstraintFuncs(self):
+        r"""Get list of constraint functions
+
+        :Call:
+            >>> funcs = opts.get_FunctionalConstraintFuncs()
+        :Inputs:
+            *opts*: :class:`OptionsDict`
+                Options interface
+        :Outputs:
+            *funcs*: :class:`list`\ [:class:`str`]
+                List of function names
+        :Versions:
+            * 2016-04-25 ``@ddalle``: v1.0
+            * 2023-05-16 ``@ddalle``: v2.0
+        """
+        # List of functions
+        funcs = []
+        # Loop through keys
+        for fn in self:
+            # Get type
+            typ = self.get_FunctionalFuncOpt(fn, "type")
+            # Check type
+            if typ == "constraint":
+                funcs.append(fn)
+        # Output
+        return funcs
+
+    # Get list of optimization functions
+    def get_FunctionalOptFuncs(self):
+        r"""Get list of objective functions
+
+        :Call:
+            >>> funcs = opts.get_FunctionalOptFuncs()
+        :Inputs:
+            *opts*: :class:`OptionsDict`
+                Options interface
+        :Outputs:
+            *funcs*: :class:`list`\ [:class:`str`]
+                List of function names
+        :Versions:
+            * 2016-04-25 ``@ddalle``: v1.0
+            * 2023-05-16 ``@ddalle``: v2.0
+        """
+        # List of functions
+        funcs = []
+        # Loop through keys
+        for fn in self:
+            # Get type
+            typ = self.get_FunctionalFuncOpt(fn, "type")
+            # Check type
+            if typ == "objective":
+                funcs.append(fn)
+        # Output
+        return funcs
+
+
 
 
 # Class for output functional settings
