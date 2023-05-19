50ec57a0b298442d3750a3b30a7487351fb881e3
Derek J. Dalle <derek.j.dalle@nasa.gov>
2023-05-19 00:38:29 -0700

    Make new test script use cape/1.1 module

--- qsub_pytest.sh
+++ qsub_pytest.sh
@@ -17,7 +17,7 @@ module use -a /home3/serogers/share/modulefiles
 module use -a /home5/ddalle/share/modulefiles
 module use -a /home3/fun3d/shared/n1337/toss3/modulefiles
 module load python3/3.9.12
-module load cape/devel-p16
+module load cape/1.1
 module load overflow/2.4b_dp
 module load FUN3D_Rome_TecIO/13.7
 module load cart3d/1.5.9
