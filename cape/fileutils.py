114436b7808d73c4d753e03d24dfc1e139ce7b8a
Derek Dalle <derek.j.dalle@nasa.gov>
2023-06-16 21:42:31 -0700

    Finish head(), tail(), and touch()

--- cape/fileutils.py
+++ cape/fileutils.py
@@ -7,9 +7,46 @@ commands or programs, but written in pure Python to remove operating
 system dependencies.
 """
 
+# Standard library
+import os
+import time
+
 
 # Default encoding
 DEFAULT_ENCODING = "utf-8"
+
+
+# Return first few lines
+def head(fname: str, n=1, encoding=DEFAULT_ENCODING):
+    r"""Get first *n* lines of a file
+
+    :Call:
+        >>> txt = head(fname, n=1)
+    :Inputs:
+        *fname*: :class:`str`
+            Name of file to read
+        *n*: {``1``} | :class:`int`
+            Number of lines to read from beginning of file
+        *encoding*: {``"utf-8"``} | :class:`str`
+            Encoding for text file
+    :Versions:
+        * 2023-06-16 ``@ddalle``: v1.0
+    """
+    # Initialize text
+    txt = ""
+    # Open file
+    with open(fname, 'r', encoding=encoding) as fp:
+        # Loop through lines
+        for _ in range(n):
+            # Read next line
+            line = fp.readline()
+            # Check for EOF
+            if line == "":
+                break
+            # Append to text
+            txt += line
+    # Output
+    return txt
 
 
 # Pure Python to tail a file
@@ -24,6 +61,7 @@ def tail(fname: str, n=1, encoding=DEFAULT_ENCODING):
         *n*: {``1``} | :class:`int`
             Number of lines to read from end of file
         *encoding*: {``"utf-8"``} | :class:`str`
+            Encoding for text file
     :Versions:
         * 2023-06-16 ``@ddalle``: v1.0
     """
@@ -55,4 +93,27 @@ def tail(fname: str, n=1, encoding=DEFAULT_ENCODING):
                 m += 1
         # File is no after last \n; read to EOF
         return fb.read().decode(encoding)
+
+
+# Create a file
+def touch(fname: str):
+    r"""Replicate ``touch``; creating new file or updating mod time
+
+    :Call:
+        >>> touch(fname)
+    :Inputs:
+        *fname*: :class:`str`
+            Name of file to create/modify
+    :Versions:
+        * 2023-06-16 ``@ddalle``: v1.0
+    """
+    # Test if file exists
+    if os.path.isfile(fname):
+        # Get current time
+        tic = time.time()
+        # Set access time and mod time
+        os.utime(fname, (tic, tic))
+    else:
+        # Create the file and close it
+        open(fname).close()
 
