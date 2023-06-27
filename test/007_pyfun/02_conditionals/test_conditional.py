75790b15be05fb448601ac05397828e3e01382e0
Jamie Meeroff <jamie.g.meeroff@nasa.gov>
2023-06-27 16:39:46 -0700

    Start pyfun test to check conditional inputs

---
+++ test/007_pyfun/02_conditionals/test_conditional.py
@@ -0,0 +1,25 @@
+
+# Third party
+import testutils
+
+# Local imports
+import cape.pyfun.cntl
+
+# List of file globs to copy into sandbox
+TEST_FILES = (
+    "pyFun.json",
+    "matrix.csv"
+)
+
+
+# Test cons in MeshFile
+@testutils.run_sandbox(__file__, TEST_FILES)
+def test_conditionals01():
+    # Instantiate
+    cntl = cape.pyfun.cntl.Cntl()
+    # Assert subssonic
+    assert cntl.opts.get_MeshFile(i=0) == "subsonic.ugrid"
+    # Assert supersonic
+    assert cntl.opts.get_MeshFile(i=18) == "supersonic.ugrid"
+
+
