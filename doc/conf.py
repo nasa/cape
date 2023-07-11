d4da456a832ada3f1795973e0c0aa03bef56eb15
Derek Dalle <derek.j.dalle@nasa.gov>
2023-07-11 11:53:20 -0700

    Remove very old testin script w/i doc/ folder

--- doc/conf.py
+++ doc/conf.py
@@ -35,22 +35,17 @@ now = datetime.datetime.now()
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
 
 # Added to clean up autodoc toctrees
 toc_object_entries = False
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
 ]
 
