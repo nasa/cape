700c12a8396e625f94742e65ec00af8c6b2afeef
Derek J. Dalle <derek.j.dalle@nasa.gov>
2023-09-26 14:29:49 -0700

    Switch the rows/columns order for 2D arrays in write_csv_dense

--- cape/attdb/ftypes/csvfile.py
+++ cape/attdb/ftypes/csvfile.py
@@ -1039,7 +1039,7 @@ class CSVFile(BaseFile, TextInterpreter):
                     ("col '%s'" % col))
                 continue
             # Get dimensions (number of rows)
-            nxj, nyj = vj.shape
+            nyj, nxj = vj.shape
             # Check dimension (number of rows)
             if nx != nxj:
                 # Skip mismatching array
@@ -1053,7 +1053,7 @@ class CSVFile(BaseFile, TextInterpreter):
                 colk = "%s[%i]" % (col, k)
                 # Save information
                 parsedcols.append(colk)
-                vals[colk] = vj[:, k]
+                vals[colk] = vj[k, :]
                 wflags.append(wflagj)
         # Number of columns
         ncol = len(parsedcols)
