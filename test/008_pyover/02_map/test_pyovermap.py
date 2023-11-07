ccd73910cefafc3012b3a08341bca51ad14e2694
Derek Dalle <derek.j.dalle@nasa.gov>
2023-11-06 22:49:17 -0800

    Add nearly empty test files for pyover @map test

---
+++ test/008_pyover/02_map/test_pyovermap.py
@@ -0,0 +1,27 @@
+
+# Third-party imports
+import testutils
+
+# Local imports
+from cape.pyover.cntl import Cntl
+
+
+# List of files to cpy
+COPY_FILES = (
+    "pyOver.json",
+    "matrix.csv",
+)
+COPY_DIRS = (
+    "common_inflow",
+)
+
+
+# Initialize
+@testutils.run_sandbox(__file__, COPY_FILES, COPY_DIRS)
+def test_meshmap01():
+    # Instantiate control class
+    cntl = Cntl()
+
+
+if __name__ == "__main__":
+    test_meshmap01()
