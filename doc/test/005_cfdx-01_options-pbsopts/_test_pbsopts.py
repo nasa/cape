eb5c8dc6c5bf4ff26feae426148cca670063512b
Derek J. Dalle <derek.j.dalle@nasa.gov>
2023-12-23 08:14:52 -0800

    Remove most test report docs

--- doc/test/005_cfdx-01_options-pbsopts/_test_pbsopts.py
+++
@@ -1,9 +0,0 @@
-# Local
-from cape.cfdx.options import pbsopts
-
-
-def test_PBSOpts():
-    # Initialize options
-    opts = pbsopts.PBSOpts(select=[1, 10], ncpus=40)
-    # Test special function
-    assert opts.get_nPBS() == 2
