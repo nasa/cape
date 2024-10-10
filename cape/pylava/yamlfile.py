78efa809816535c799622a3863854a05a1a4bbea
Derek J. Dalle <derek.j.dalle@nasa.gov>
2024-10-10 15:45:14 -0700

    Add specialized get/set commands for LAVA YAML file

--- cape/pylava/yamlfile.py
+++ cape/pylava/yamlfile.py
@@ -82,11 +82,34 @@ class RunYAMLFile(OptionsDict):
         # Save value
         secopts[opt.lower()] = v
 
-    # Get angle of attack
-    def get_alpha(self) -> float:
-        return self.get_lava_subopt("referenceconditions", "alpha")
+    def get_refcond(self, opt: str) -> Any:
+        # Fixed section name
+        sec = "referenceconditions"
+        # Use generic method
+        return self.get_lava_subopt(sec, opt)
 
-    # Set angle of attack
+    def set_refcond(self, opt: str, v: Any):
+        # Fixed section name
+        sec = "referenceconditions"
+        # Use generic method
+        self.set_lava_subopt(sec, opt, v)
+
+    def get_alpha(self) -> float:
+        r"""Get the angle of attack
+
+        :Call:
+            >>> alpha = opts.get_alpha()
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+        :Outputs:
+            *alpha*: :class:`float`
+                Angle of attack [deg]
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        return self.get_refcond("alpha")
+
     def set_alpha(self, alpha: float):
         r"""Set the angle of attack
 
@@ -96,8 +119,132 @@ class RunYAMLFile(OptionsDict):
             *opts*: :class:`RunYAMLFile`
                 LAVA YAML file interface
             *alpha*: :class:`float`
-                Angle of attack
+                Angle of attack [deg]
         :Versions:
             * 2024-10-10 ``@ddalle``: v1.0
         """
-        self.set_lava_subopt("referenceconditions", "alpha")
+        self.set_refcond("alpha", alpha)
+
+    def get_gamma(self) -> float:
+        r"""Get the ratio of specific heats (*gamma*)
+
+        :Call:
+            >>> gam = opts.get_gamma()
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+        :Outputs:
+            *gam*: :class:`float`
+                Ratio of specific heats
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        return self.get_refcond("gamma")
+
+    def set_gamma(self, gam: float):
+        r"""Set the ratio of specific heats (*gamma*)
+
+        :Call:
+            >>> opts.get_gam(gam)
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+            *gam*: :class:`float`
+                Ratio of specific heats
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        self.set_refcond("gamma", gam)
+
+    def get_pressure(self) -> float:
+        r"""Get the freestream/farfield pressure [Pa]
+
+        :Call:
+            >>> p = opts.get_pressure()
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+        :Outputs:
+            *p*: :class:`float`
+                Static pressure [Pa]
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        return self.get_refcond("pressure")
+
+    def set_pressure(self, p: float):
+        r"""Set the freestream/farfield pressure [Pa]
+
+        :Call:
+            >>> opts.set_pressure(p)
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+            *p*: :class:`float`
+                Static pressure [Pa]
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        self.set_refcond("pressure", p)
+
+    def get_temperature(self) -> float:
+        r"""Get the freestream/farfield temperature [K]
+
+        :Call:
+            >>> tinf = opts.get_temperature()
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+        :Outputs:
+            *tinf*: :class:`float`
+                Static temperature [K]
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        return self.get_refcond("temperature")
+
+    def set_temperature(self, tinf: float):
+        r"""Get the freestream/farfield temperature [K]
+
+        :Call:
+            >>> opts.set_temperature(tinf)
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+            *tinf*: :class:`float`
+                Static temperature [K]
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        self.set_refcond("temperature", tinf)
+
+    def get_umag(self) -> float:
+        r"""get the velocity magnitude [m/s]
+
+        :Call:
+            >>> u = opts.get_umag()
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+        :Outputs:
+            *u*: :class:`float`
+                Velocity magnitude
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        return self.get_refcond("umag")
+
+    def set_umag(self, u: float):
+        r"""Set the velocity magnitude [m/s]
+
+        :Call:
+            >>> opts.set_umag(u)
+        :Inputs:
+            *opts*: :class:`RunYAMLFile`
+                LAVA YAML file interface
+            *u*: :class:`float`
+                Velocity magnitude
+        :Versions:
+            * 2024-10-10 ``@ddalle``: v1.0
+        """
+        self.set_refcond("umag", u)
