f15c47258db85d91518de5b030263059117ad947
Derek J. Dalle <derek.j.dalle@nasa.gov>
2024-12-04 09:14:09 -0800

    Customize v1.2 test script for v1.2 branch

--- test/qsub_pytest.sh
+++ test/qsub_pytest.sh
@@ -19,7 +19,7 @@ module use -a /home3/serogers/share/modulefiles
 module use -a /home5/ddalle/share/modulefiles
 module use -a /home3/fun3d/shared/n1337/toss3/modulefiles
 module load python3/3.11.5
-module load cape/devel
+module load cape/1.2
 module load aflr3/16.27.3
 module load overflow/2.4b_dp
 module load f3d_Rome_TecIO/13.7
@@ -36,14 +36,5 @@ fi
 
 # Switch to python 3.6
 module swap python3 python3/3.6.8
-python3 drive_pytest.py push
-
-# Check for failure
-if [[ "$?" != "0" ]]; then
-    exit 0
-fi
-
-# Switch to python 3.9
-module swap python3 python3/3.9.12
 python3 drive_pytest.py push
 
