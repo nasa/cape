bdc9ac62538152f955cd1c536cef0bc91f5b95c9
Derek Dalle <derek.j.dalle@nasa.gov>
2024-12-30 13:58:02 -0800

    Shift pyfun to the new-style CLI

--- cape/pyfun/cli.py
+++ cape/pyfun/cli.py
@@ -9,55 +9,40 @@ executed whenever ``pyfun`` is used.
 """
 
 # Standard library modules
-import sys
+from typing import Optional
 
-# CAPE modules
-from .. import argread
-from .. import text as textutils
-from .cli_doc import PYFUN_HELP
+# Local imports
+from .casecntl import CaseRunner
+from .cntl import Cntl
+from ..cfdx import cli
 
 
-# Primary interface
-def main():
+# Customized parser
+class PyfunFrontDesk(cli.CfdxFrontDesk):
+    # Attributes
+    __slots__ = ()
+
+    # Identifiers
+    _name = "pyfun"
+
+    # Custom classes
+    _cntl_cls = Cntl
+    _runner_cls = CaseRunner
+
+
+# New-style CLI
+def main(argv: Optional[list] = None) -> int:
     r"""Main interface to ``pyfun``
 
     This turns ``sys.argv`` into Python arguments and calls
-    :func:`cape.pyfun.cntl.Cntl.cli`. 
+    :func:`cape.pyfun.cntl.Cntl.cli`.
 
     :Call:
         >>> main()
     :Versions:
-        * 2021-03-03 ``@ddalle``: Version 1.0
+        * 2021-03-03 ``@ddalle``: v1.0
+        * 2024-12-30 ``@ddalle``: v2.0; use ``argread``
     """
-    # Parse inputs
-    a, kw = argread.readflagstar(sys.argv)
-    # Check for args
-    if len(a) == 0:
-        # No command, doing class pyfun behavior
-        cmd = None
-    else:
-        cmd = a[0]
-
-    # Check for "run_fun3d"
-    if cmd in {"run_fun3d", "run"}:
-        # Run case in this folder
-        from .casecntl import run_fun3d
-        # Run and exit
-        return run_fun3d()
-
-    # Check for a help flag
-    if kw.get('h') or kw.get("help"):
-        # Display help
-        print(textutils.markdown(PYFUN_HELP))
-        return
-
-    # Get file name
-    fname = kw.get('f', "pyFun.json")
-
-    # Try to read it
-    from .cntl import Cntl
-    cntl = Cntl(fname)
-
-    # Call the command-line interface
-    cntl.cli(*a, **kw)
+    # Output
+    return cli.main_template(PyfunFrontDesk, argv)
 
