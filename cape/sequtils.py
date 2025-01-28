0fb58f7ef94970406c35d4bed931a291018bcd16
Derek Dalle <derek.j.dalle@nasa.gov>
2025-01-28 12:58:22 -0800

    Create an Iter1D class to just store and find iterations and vals

--- cape/sequtils.py
+++ cape/sequtils.py
@@ -9,12 +9,17 @@ variable time steps in CFD results.
 """
 
 # Standard library
+from typing import Optional
 
 # Third-party
 import numpy as np
 
 # Local imports
 from .argread._vendor.kwparse import KwargParser
+
+
+# Type arrays
+INT_TYPES = (int, np.integer)
 
 
 # Options for processing
@@ -24,13 +29,74 @@ class SequenceKwargs(KwargParser):
 
     # Permissible options
     _optlist = (
-        "NStats",
-        "NStatsMax",
+        "DeltaN",
+        "NFirst",
+        "NLast",
         "NMax",
-        "NMin",
+        "NStats",
     )
 
     # Aliases
     _optmap = {
+        "NAvg": "NStats",
+        "NMin": "NFirst",
+        "NMaxStats": "NMax",
+        "NStatsMax": "NMax",
+        "dN": "DeltaN",
+        "deltaN": "DeltaN",
+        "dn": "DeltaN",
         "nAvg": "NStats",
+        "nFirst": "NFirst",
+        "nLast": "NLast",
+        "nMax": "NMax",
+        "nMaxStats": "NMax",
+        "nMin": "NFirst",
+        "nStats": "NStats",
+        "nStatsMax": "NMax",
+        "navg": "NStats",
+        "nfirst": "NFirst",
+        "nlast": "NLast",
+        "nmax": "NMax",
+        "nmaxstats": "NMax",
+        "nmin": "NFirst",
+        "nstats": "NStats",
+        "nstatsmax": "NMax",
     }
+
+    # Types
+    _opttypes = {
+        "DeltaN": INT_TYPES,
+        "NFirst": INT_TYPES,
+        "NLast": INT_TYPES,
+        "NMax": INT_TYPES,
+        "NStats": INT_TYPES,
+    }
+
+
+# Iterative histories
+class Iter1D(object):
+    # Attributees
+    __slots__ = (
+        "i",
+        "v",
+        "description",
+    )
+
+    # Initialize
+    def __init__(
+            self,
+            v: np.ndarray,
+            i: Optional[np.ndarray] = None,
+            description: Optional[str] = None):
+        # Initialize iterations
+        if i is None:
+            i = np.arange(v.size)
+        # Check sizes
+        if i.size != v.size:
+            raise ValueError(
+                f"Iterative history for {description} has mismatching size; "
+                f"i: {i.size}; v: {v.size}")
+        # Save
+        self.i = i
+        self.v = v
+
