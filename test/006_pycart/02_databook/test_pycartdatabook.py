16d07d5254308be38f75c200df5af009d41e520d
aburkhea <aaron.c.burkhead@nasa.gov>
2025-02-05 17:13:11 -0800

    Add pycart update/delete fm dbook test

---
+++ test/006_pycart/02_databook/test_pycartdatabook.py
@@ -0,0 +1,56 @@
+
+# Standard Library
+import os
+import sys
+import shutil
+
+# Third-party
+import testutils
+
+
+# Test Files
+TEST_FILES = (
+    "pyCart.json",
+    "matrix.csv",
+    os.path.join("poweroff", "m1.5a0.0b0.0", "bullet_no_base.dat"),
+    os.path.join("poweroff", "m1.5a0.0b0.0", "case.json"),
+    os.path.join("poweroff", "m1.5a0.0b0.0", "history.dat"),
+    os.path.join("poweroff", "m1.5a0.0b0.0", "run.00.200"),
+    "test.[0-9][0-9].out"
+)
+
+
+@testutils.run_sandbox(__file__, TEST_FILES)
+def test_updatedatabookfm():
+    # Split update command and add `-m` prefix
+    cmdlist = [sys.executable, "-m", "cape.pycart", "-I", "0", "--fm"]
+    # Run command
+    stdout, _, _ = testutils.call_o(cmdlist)
+    # Location of output databook
+    dbout = os.path.join("data/aero_bullet_no_base.csv")
+    # Compare output databook with reference result
+    result = testutils.compare_files(dbout, "test.01.out")
+    # Test updated FM Databook
+    assert result.line1 == result.line2
+
+
+@testutils.run_sandbox(__file__, TEST_FILES)
+def test_deletecasesfm():
+    os.mkdir("data")
+    # Use test.01.out as existing databook
+    shutil.copy("test.01.out", os.path.join("data", "aero_bullet_no_base.csv"))
+    # Split delete command and add `-m` prefix
+    cmdlist = [sys.executable, "-m", "cape.pycart", "-I", "0", "--fm",
+               "--delete"]
+    # Run command
+    stdout, _, _ = testutils.call_o(cmdlist)
+    # Location of output databook
+    dbout = os.path.join("data/aero_bullet_no_base.csv")
+    # Location of old databook
+    dbold = os.path.join("data/aero_bullet_no_base.csv.old")
+    # Compare output databook with reference result
+    result = testutils.compare_files(dbout, "test.02.out")
+    # Test deleted FM Databook
+    assert result.line1 == result.line2
+    # Test old databook exists
+    assert os.path.exists(dbold)
