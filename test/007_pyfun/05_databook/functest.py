d5738e02ea33fab618687cb1b79c7ae4199357c4
aburkhea <aaron.c.burkhead@nasa.gov>
2025-02-12 11:16:41 -0800

    Clean up functest module

--- test/007_pyfun/05_databook/functest.py
+++ test/007_pyfun/05_databook/functest.py
@@ -1,32 +1,12 @@
 
-# Standard library
-import os
-
 # Third-party
 import cape.pltfile
 import numpy as np
-
-
-XCOLS = (
-    "mach",
-    "alpha",
-    "beta",
-    "q",
-    "T",
-    "config",
-    "Label"
-)
-
-COEFFS = (
-    "CA",
-    "CY",
-    "CN"
-)
 
 
 def f3dfunc(cntl, i):
     plt = cape.pltfile.Plt("arrow_tec_boundary_timestep200.plt")
     v = {}
     for i, zone in enumerate(plt.Zones):
-        v[zone.split(" ")[-1] + ".cp_std"] = np.std(plt.q[i][:,-2])
+        v[zone.split(" ")[-1] + ".cp_std"] = np.std(plt.q[i][:, -2])
     return v
