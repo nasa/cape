749ac1bbb2b02f68a2bca66680c3d2ad46db3525
aburkhea <aaron.c.burkhead@nasa.gov>
2025-02-26 13:59:57 -0800

    Add ll and pyfunc dbook tests to pyover

--- test/008_pyover/03_databook/test_001_pyoverdatabook.py
+++ test/008_pyover/03_databook/test_001_pyoverdatabook.py
@@ -1,6 +1,5 @@
 
 # Standard library
-import sys
 import os
 import shutil
 
@@ -11,10 +10,14 @@ import testutils
 import cape.pyover.cntl as pocntl
 
 
+# Dir to case files
+CASEDIR = os.path.join("poweroff", "m0.8a4.0b0.0")
+
 # List of file globs to copy into sandbox
 TEST_FILES = (
     "pyOver.json",
     "matrix.csv",
+    "oflfunc",
     "test.[0-9][0-9].out"
 )
 TEST_DIRS = (
@@ -33,6 +36,40 @@ KW1 = {
 KW2 = {
     "I": [1],
     "fm": "bullet_no_base",
+    "restart": True,
+    "start": True,
+    "delete": True,
+    "__replaced__": []
+}
+
+KW3 = {
+    "I": [1],
+    "ll": True,
+    "restart": True,
+    "start": True,
+    "__replaced__": []
+}
+
+KW4 = {
+    "I": [1],
+    "ll": True,
+    "restart": True,
+    "start": True,
+    "delete": True,
+    "__replaced__": []
+}
+
+KW5 = {
+    "I": [1],
+    "dbpyfunc": True,
+    "restart": True,
+    "start": True,
+    "__replaced__": []
+}
+
+KW6 = {
+    "I": [1],
+    "dbpyfunc": True,
     "restart": True,
     "start": True,
     "delete": True,
@@ -73,3 +110,82 @@ def test_deletecasesfm():
     assert result.line1 == result.line2
     # Test old databook exists
     assert os.path.exists(dbold)
+
+
+@testutils.run_sandbox(__file__, TEST_FILES, TEST_DIRS)
+def test_updatedatabookll():
+    fll = os.path.join(CASEDIR, "lineload", "LineLoad_bullet_total_LL.dlds")
+    # Ensure modifcation time of line load file is after triq file
+    os.utime(fll, None)
+    # Get cntl
+    cntl = pocntl.Cntl()
+    # Call dbook updater
+    cntl.UpdateLL(**KW3)
+    # Location of output databook
+    dbout = os.path.join("data/ll_bullet_total_LL.csv")
+    # Compare output databook with reference result
+    result = testutils.compare_files(dbout, "test.03.out")
+    # Test updated FM Databook
+    assert result.line1 == result.line2
+
+
+@testutils.run_sandbox(__file__, TEST_FILES, TEST_DIRS)
+def test_deletecasesll():
+    fll = os.path.join(CASEDIR, "lineload", "LineLoad_bullet_total_LL.dlds")
+    # Ensure modifcation time of line load file is after triq file
+    os.utime(fll, None)
+    os.mkdir("data")
+    # Use test.01.out as existing databook
+    shutil.copy("test.03.out", os.path.join("data",
+                                            "ll_bullet_total_LL.csv"))
+    # Get cntl
+    cntl = pocntl.Cntl()
+    # Call dbook updater
+    cntl.UpdateLL(**KW4)
+    # Location of output databook
+    dbout = os.path.join("data/ll_bullet_total_LL.csv")
+    # Location of old databook
+    dbold = os.path.join("data/ll_bullet_total_LL.csv.old")
+    # Compare output databook with reference result
+    result = testutils.compare_files(dbout, "test.04.out")
+    # Test deleted FM Databook
+    assert result.line1 == result.line2
+    # Test old databook exists
+    assert os.path.exists(dbold)
+
+
+@testutils.run_sandbox(__file__, TEST_FILES, TEST_DIRS)
+def test_updatedatabookfunc():
+    # Get cntl
+    cntl = pocntl.Cntl()
+    # Call dbook updater
+    cntl.UpdateDBPyFunc(**KW5)
+    # Location of output databooks
+    dbout1 = os.path.join("data/pyfunc_functest.csv")
+    # Compare output databook with reference result
+    result = testutils.compare_files(dbout1, "test.05.out")
+    # Test updated FM Databook
+    assert result.line1 == result.line2
+
+
+@testutils.run_sandbox(__file__, TEST_FILES, TEST_DIRS)
+def test_deletecasesfunc():
+    os.mkdir("data")
+    # Use test.01.out as existing databook
+    shutil.copy("test.05.out",
+                os.path.join("data", "pyfunc_functest.csv"))
+    # Get cntl
+    cntl = pocntl.Cntl()
+    # Call dbook updater
+    cntl.UpdateDBPyFunc(**KW6)
+    # Location of output databook
+    dbout = os.path.join("data/pyfunc_functest.csv")
+    # Location of old databook
+    dbold = os.path.join("data/pyfunc_functest.csv.old")
+    # Compare output databook with reference result
+    result = testutils.compare_files(dbout, "test.06.out")
+    # Test deleted FM Databook
+    assert result.line1 == result.line2
+    # Test old databook exists
+    assert os.path.exists(dbold)
+
