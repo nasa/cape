24e0df8482c90f696b408017ec9a129a94f2762c
Derek J. Dalle <derek.j.dalle@nasa.gov>
2025-03-28 18:07:20 -0700

    Add some output tests for PreShellCmds test

--- test/902_pyfun/004_hooks/test_pyfunchooks.py
+++ test/902_pyfun/004_hooks/test_pyfunchooks.py
@@ -8,7 +8,6 @@ import testutils
 
 # Local imports
 from cape.pyfun.cntl import Cntl
-from cape.pyfun import cli
 
 
 # List of file globs to copy into sandbox
@@ -18,6 +17,7 @@ TEST_FILES = (
     "fun3d.nml",
     "matrix.csv",
     "bullet-*",
+    "preshell.sh",
 )
 
 
@@ -26,7 +26,20 @@ TEST_FILES = (
 def test_01_run():
     # Instantiate
     cntl = Cntl()
+    # Case to run
+    i = 8
     # Run first case
-    cntl.SubmitJobs(I="8")
+    cntl.SubmitJobs(I=str(i))
+    # Name of case
+    frun = cntl.x.GetFullFolderNames(i)
+    # Path to preshell script and output file
+    shellfile = os.path.join(frun, "preshell.sh")
+    logfile = os.path.join(frun, "cape-preshellcmd.log")
+    # Check that file exists
+    assert os.path.isfile(shellfile)
+    assert os.path.isfile(logfile)
+    # Check length
+    lines = open(logfile).readlines()
+    assert len(lines) == 2
 
 
