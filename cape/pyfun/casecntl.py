2aef8f965f8ffb4d138aaeeb3a733f786636f465
Aaron Christopher Burkhead <aaron.c.burkhead@nasa.gov>
2025-04-17 11:22:19 -0700

    Add flow2ufunc worker

--- cape/pyfun/casecntl.py
+++ cape/pyfun/casecntl.py
@@ -744,6 +744,74 @@ class CaseRunner(casecntl.CaseRunner):
         # Write it
         mesh.write(fname_vplt)
 
+    def flow2ufunc(self, **kw):
+        r"""Convert most recent ``.flow`` file to SimSys ufunc file
+
+        :Call:
+            >>> runner.flow2ufunc()
+        :Inputs:
+            *runner*: :class:`CaseRunner`
+                Controller to run one case of solver
+            *add-mach*: {``True``} | ``False``
+                Option to calculate Mach number and add it to PLT file
+            *add-cp*: {``True``} | ``False``
+                Option to add pressure coefficient and to PLT file
+        :Versions:
+            * 2025-04-04 ``@ddalle``: v1.0
+        """
+        # Get restart iteration
+        n = self.get_restart_iter()
+        # Get project name
+        proj = self.get_project_rootname()
+        # Name of flow file
+        fname_flow = f"{proj}.flow"
+        # Exit if no current flow file and finished writing
+        if not os.path.isfile(fname_flow):
+            return
+        elif os.path.getsize(fname_flow) < 1000:
+            return
+        elif time.time() - os.path.getmtime(fname_flow) < 5.0:
+            # Get time for print message
+            dt = time.time() - os.path.getmtime(fname_flow)
+            # Log result
+            self.log_verbose(
+                f"FUN3D flow file '{fname_flow}' is only {dt:.1f} s old; "
+                "might still be in I/O")
+            return
+        # Get mesh file extension
+        grid_ext = self.get_grid_extension()
+        bc_ext = self.get_bc_extension()
+        # Search for grids
+        pat = f"{proj}.*{grid_ext}"
+        meshfiles = self.search_workdir(pat, regex=False, links=True)
+        # Exit if no mesh files
+        if len(meshfiles) == 0:
+            return
+        # Use latest mesh file
+        fname_mesh = meshfiles[-1]
+        fname_bc = f"{proj}.{bc_ext}"
+        # Check for mapbc file
+        fname_bc = fname_bc if os.path.isfile(fname_bc) else None
+        # Name of output file
+        fname_vufnc = f"{proj}_volume_timestep{n}.lb8.ufunc"
+        # Exit if that file already exists
+        if os.path.isfile(fname_vufnc):
+            return
+        # Update
+        self.log_verbose(
+            f"Convert {fname_mesh} + {fname_flow} -> {fname_vufnc}")
+        # Read mesh
+        mesh = umesh.Umesh(fname_mesh, mapbc=fname_bc)
+        # Read flow file
+        mesh.read_fun3d_flow(fname_flow)
+        # Add additional parameters
+        if kw.get("add-mach", True):
+            mesh.add_mach()
+        if kw.get("add-cp", True):
+            mesh.add_cp()
+        # Write it
+        mesh.write(fname_vufnc)
+
    # --- File manipulation ---
     # Rename/move files prior to running phase
     def prepare_files(self, j: int):
