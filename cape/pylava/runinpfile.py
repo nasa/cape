53a7a11a5c4e1146041e9cb9cc95ff4087233e40
Derek J. Dalle <derek.j.dalle@nasa.gov>
2025-05-06 15:21:24 -0700

    Start class for LAVA-Cartesian input file

---
+++ cape/pylava/runinpfile.py
@@ -0,0 +1,18 @@
+r"""
+:mod:`cape.pylava.runinpfile`: Interface to LAVA-Cartesian input files
+=======================================================================
+
+This module provides the class :class:`CartInputFile`, which is used to
+parse and modify the C-like input file to LAVA-Cartesian
+"""
+
+# Standard library
+
+# Third-party
+
+# Local imports
+
+
+# Base class
+class CartInputFile(dict):
+    __slots__ = ()
