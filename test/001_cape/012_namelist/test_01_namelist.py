ea2cf0aba66bd2c18891c0ec1439af8fdc34d9fc
aburkhea <aaron.c.burkhead@nasa.gov>
2025-06-04 15:30:03 -0700

    Add more coverage to namelist test

--- test/001_cape/012_namelist/test_01_namelist.py
+++ test/001_cape/012_namelist/test_01_namelist.py
@@ -7,6 +7,7 @@ import cape.filecntl.namelist as nml
 
 NML_FILE = "bullet.nml"
 
+
 # Basic test
 @testutils.run_testdir(__file__)
 def test_01_namelist():
@@ -18,4 +19,18 @@ def test_01_namelist():
     assert len(b1.SectionNames) == 8
     # Test content
     assert b1.GetVar('FLOINP', 'FSMACH') == 0.8
-
+    # Test set
+    b1.SetVar('FLOINP', 'FSMACH', 0.9)
+    assert b1.GetVar('FLOINP', 'FSMACH') == 0.9
+    # Test return dict
+    d1 = b1.ReturnDict()
+    assert d1
+    # Test apply dict
+    b1.ApplyDict(d1)
+    assert b1
+    # Test Copy
+    b2 = b1.Copy(None)
+    assert b2
+    # Test set var for non-existing sec
+    b1.SetVar("TEST", "Test1", 1.1, k=[0, 1, 2])
+    assert b1.GetVar("TEST", "Test1", k=[0, 1, 2]) == 1.1
