356efd1e570e7b893b51c7e8cc9197df22cf8034
Derek J. Dalle <derek.j.dalle@nasa.gov>
2025-06-24 14:31:26 -0700

    Make more changes to pycart's iteration cutoffs
    Cart3D 1.6 seems to be extremely buggy for easy problems. It will not run more
    iterations after reaching a residual drop, even if you explicitly change the
    target for the restart to require more orders of reduction

--- cape/pycart/casecntl.py
+++ cape/pycart/casecntl.py
@@ -262,16 +262,7 @@ class CaseRunner(casecntl.CaseRunner):
                 PS.UpdateIterations()
                 PS.WriteHist()
         # Reset PhaseIters if early exit
-        if self.checkx_phase(j):
-            # Get current phase cutoff
-            nreq = rc.get_PhaseIters(j)
-            # Get current iters
-            nrun = self.getx_iter()
-            nrun = 0 if nrun is None else int(nrun)
-            # Reset if needed
-            if nrun < nreq:
-                rc.set_PhaseIters(nrun, j)
-                self.write_case_json(rc)
+        self._reset_phase_iters(j)
         # Output
         return ierr
 
@@ -322,12 +313,12 @@ class CaseRunner(casecntl.CaseRunner):
                 # Increase reference for averaging.
                 n0 += rc.get_it_start(j)
                 # Modified command
-                cmdi = cmdgen.flowCart(opts=rc, i=j, n=n)
+                cmdi = cmdgen.flowCart(opts=rc, j=j, n=n)
                 # Reset averaging settings
                 rc.set_it_avg(it_avg)
             else:
                 # Normal stops every *it_avg* iterations.
-                cmdi = cmdgen.flowCart(opts=rc, i=j, n=n)
+                cmdi = cmdgen.flowCart(opts=rc, j=j, n=n)
             # Run the command for *it_avg* iterations.
             ierr = self.callf(cmdi, f='flowCart.out', e="flowCart.err")
             # Automatically determine the best check file to use.
@@ -361,6 +352,8 @@ class CaseRunner(casecntl.CaseRunner):
                 PS.WriteHist()
         except Exception:
             pass
+        # Reset PhaseIters if early exit
+        self._reset_phase_iters(j)
         # Output
         return ierr
 
@@ -392,7 +385,7 @@ class CaseRunner(casecntl.CaseRunner):
             # Get the number of previous steady steps
             n = self.get_steady_iter()
         # Call flowCart directly.
-        cmdi = cmdgen.flowCart(opts=rc, i=j, n=n)
+        cmdi = cmdgen.flowCart(opts=rc, j=j, n=n)
         # Run the command.
         ierr = self.callf(cmdi, f='flowCart.out', e="flowCart.err")
         # Check for point sensors
@@ -401,6 +394,8 @@ class CaseRunner(casecntl.CaseRunner):
             PS = pointsensor.CasePointSensor()
             PS.UpdateIterations()
             PS.WriteHist()
+        # Update phase iteration cutoffs
+        self._reset_phase_iters(j)
         # Return code
         return ierr
 
@@ -863,6 +858,7 @@ class CaseRunner(casecntl.CaseRunner):
         # Check if adaptive
         return not rc.get_opt("Adaptive", j)
 
+   # --- Early-exit phase tools ---
     # Solver-specifc phase
     def checkx_phase(self, j: int) -> bool:
         r"""Apply solver-specific checks for phase *j*
@@ -898,6 +894,31 @@ class CaseRunner(casecntl.CaseRunner):
         else:
             # No other checks
             return True
+
+    # Reset PhaseIters
+    def _reset_phase_iters(self, j: int):
+        # Read options
+        rc = self.read_case_json()
+        # Reset PhaseIters if early exit
+        if self.checkx_phase(j):
+            # Get current phase cutoff
+            nreq = rc.get_PhaseIters(j)
+            # Get current iters
+            nrun = self.getx_iter()
+            nrun = 0 if nrun is None else int(nrun)
+            # Reset if needed
+            if nrun >= nreq:
+                return
+            # Loop through this phase and followers
+            for jb in range(j, 1 + self.get_last_phase()):
+                # Get required iter
+                nj = rc.get_PhaseIters(jb)
+                # New required iter
+                njmax = max(0, (nj - nreq) + nrun)
+                # Set to lower option
+                rc.set_PhaseIters(min(nj, njmax), jb)
+            # Update ``case.json``
+            self.write_case_json(rc)
 
    # --- Local data ---
     # Read Components.i.tri
