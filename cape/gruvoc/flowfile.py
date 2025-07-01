8470f4576f2e14523600eae054fbaf168598fc23
Derek Dalle <derek.j.dalle@nasa.gov>
2025-06-30 23:44:45 -0700

    Update TAVG file reader for generic_gas_path
    Has some potential for improvement, but it should be good for freestream air
    cases. Adds T[K] and W[kg/kmol] to what compressible reports.

--- cape/gruvoc/flowfile.py
+++ cape/gruvoc/flowfile.py
@@ -190,8 +190,10 @@ def _read_fun3d_tavg(
     fp.seek(8)
     # Read number of iterations in average
     mesh.niter, = fromfile_lb4_i(fp, 1)
-    # Skip 8 bytes; these may be important?
-    fp.seek(8, 1)
+    # Read version
+    ver, = fromfile_lb4_i(fp, 1)
+    # Skip 1 int32, should be ``6``; may be important?
+    fp.seek(4, 1)
     # Read number of states
     nq, = fromfile_lb4_i(fp, 1)
     mesh.nq = nq
@@ -218,13 +220,29 @@ def _read_fun3d_tavg(
     # Inversion
     bnode = np.argsort(jnode)
     # Save variable names
-    if nq == 5:
+    if ver == 1:
+        # State variables
         mesh.qvars = [
             "rho", "u", "v", "w", "p",
         ]
-    # Read RMS and then average state
-    q = fromfile_lb8_f(fp, 2*nnode*nq).reshape((nnode, 2*nq))
-    qavg = q[:, :nq]
+        # Read RMS and then average state
+        q = fromfile_lb8_f(fp, 2*nnode*nq).reshape((nnode, 2*nq))
+        qavg = q[:, :nq]
+    elif ver == 2:
+        # Selected variables; many on offer
+        mesh.qvars = [
+            "rho", "u", "v", "w", "p", "T", "W",
+        ]
+        # Freestream Mach number
+        mach = mesh.qinf[0]
+        # Read all 32 cols
+        q = fromfile_lb8_f(fp, nnode*32).reshape((nnode, 32))
+        # Downselect
+        qavg = q[:, [0, 1, 2, 3, 4, 6, 9]]
+        # Renormalize velocities; u/Uinf -> u/ainf
+        qavg[:, [1, 2, 3]] *= mach
+        # Renormalize pressure; p/g*Minf^2*pinf -> p/g*pinf
+        qavg[:, 4] *= (mach*mach)
     # Reorder and save it
     mesh.q = qavg[bnode, :]
 
