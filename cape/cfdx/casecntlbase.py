68d25cf618ec04fe524eaa88e9e0bc95b31cfb2d
Derek J. Dalle <derek.j.dalle@nasa.gov>
2025-07-23 15:09:18 -0700

    Start somewhat-crazy read_dex() effort in CaseCntl

--- cape/cfdx/casecntlbase.py
+++ cape/cfdx/casecntlbase.py
@@ -34,8 +34,87 @@ CONDITIONS_FILE = "conditions.json"
 REGEX_RUNFILE = re.compile("run.([0-9][0-9]+).([0-9]+)")
 
 
+# Meta class to merge _dex_cls
+class MetaCaseRunner(type):
+    r"""Metaclass for :class:`CaseRunner`
+
+    This metaclass ensures that new subclasses of :class:`CaseRunner`,
+    for example in :mod:`cape.pyfun` or :mod:`cape.pylava`, merge their
+    :attr:`_dex_cls` class attribute values.
+    """
+    def __new__(metacls, name: str, bases: tuple, namespace: dict):
+        r"""Initialize a new subclass, but combine ``_dex-cls`` attr
+
+        :Call:
+            >>> cls = metacls.__new__(name, bases, namespace)
+        :Inputs:
+            *metacls*: :class:`type`
+                The :class:`MetaArgReader` metaclass
+            *name*: :class:`str`
+                Name of new class being created
+            *bases*: :class:`tuple`\ [:class:`type`]
+                Bases for new class
+            *namespace*: :class:`dict`
+                Attributes, methods, etc. for new class
+        :Outputs:
+            *cls*: :class:`type`
+                New class using *metacls* instead of :class:`type`
+        """
+        # Initialize the new class
+        cls = type.__new__(metacls, name, bases, namespace)
+        # Return the new class
+        return cls
+
+    @classmethod
+    def combine_dex_cls(metacls, clsj: type, cls: type):
+        r"""Combine the ``_dex_cls`` from a class and one of its bases
+
+        :Call:
+            >>> metacls.combine_dex_cls(clsj, cls)
+        :Inputs:
+            *metacls*: :class:`type`
+                The :class:`MetaArgReader` metaclass
+            *clsj*: :class:`type`
+                Parent class (basis) to combine into *cls*
+            *cls*: :class:`type`
+                New class in which to save combined attributes
+        """
+        metacls.combine_dict(clsj, cls, "_dex_cls")
+
+    @classmethod
+    def combine_dict(metacls, clsj: type, cls: type, attr: str):
+        r"""Combine one dict-like class attribute of *clsj* and *cls*
+
+        :Call:
+            >>> metacls.combine_dict(clsj, cls, attr)
+        :Inputs:
+            *metacls*: :class:`type`
+                The :class:`MetaArgReader` metaclass
+            *clsj*: :class:`type`
+                Parent class (basis) to combine into *cls*
+            *cls*: :class:`type`
+                New class in which to save combined attributes
+            *attr*: :class:`str`
+                Name of attribute to combine
+        """
+        # Get initial properties
+        vj = getattr(clsj, attr, None)
+        vx = cls.__dict__.get(attr)
+        # Check for both
+        qj = isinstance(vj, dict)
+        qx = isinstance(vx, dict)
+        if not (qj and qx):
+            return
+        # Copy dict from basis
+        combined_dict = dict(vj)
+        # Combine results
+        combined_dict.update(vx)
+        # Save combined list
+        setattr(cls, attr, combined_dict)
+
+
 # Definition
-class CaseRunnerBase(ABC):
+class CaseRunnerBase(ABC, metaclass=MetaCaseRunner):
     r"""Abstract base class for :class:`cape.cfdx.casecntl.CaseRunner`
 
     The main purpose for this class is to provide useful type
@@ -52,6 +131,7 @@ class CaseRunnerBase(ABC):
         * :attr:`_logprefix`
         * :attr:`_rc_cls`
         * :attr:`_archivist_cls`
+        * :attr:`_dex_cls`
     """
     # Maximum number of starts
     _nstart_max = 100
@@ -69,6 +149,8 @@ class CaseRunnerBase(ABC):
     _rc_cls = RunControlOpts
     #: Class for case archiving instances
     _archivist_cls = CaseArchivist
+    #: Classes for extracting types of data from case
+    _dex_cls = {}
 
     # Read ``case.json``
     @abstractmethod
