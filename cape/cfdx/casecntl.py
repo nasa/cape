6541dac97c969452776d80eb2acd43ed404bddb5
Derek Dalle <derek.j.dalle@nasa.gov>
2025-08-05 17:41:14 -0700

    Finish initial draft of CaseRunner.read_dex() w/ transformations

--- cape/cfdx/casecntl.py
+++ cape/cfdx/casecntl.py
@@ -2425,6 +2425,7 @@ class CaseRunner(CaseRunnerBase):
         return fm.GetStats(na, nb)
 
    # --- Readers ---
+    # Read raw data for DataBook component
     def read_dex(self, comp: str) -> DataKit:
         r"""Read a data component
 
@@ -2444,18 +2445,25 @@ class CaseRunner(CaseRunnerBase):
         compids = _listify(compid)
         # Loop through components
         for j, compj in enumerate(compids):
+            # Check for negative
+            negj = compj.startswith("-")
+            # Strip negative sign
+            compj = compj.lstrip('-')
             # Read component
             dbj = self.read_dex_element(comp, compj)
             # Transformations
-            ...
+            self.transform_dex(comp, dbj)
             # Add or initialize
             if j == 0:
                 db = dbj
+            elif negj:
+                dbj -= dbj
             else:
                 db += dbj
         # Output
         return db
 
+    # Read one element of a data extraction
     def read_dex_element(self, comp: str, compid: str) -> DataKit:
         r"""Read one element of a data extracter component
 
@@ -2478,13 +2486,8 @@ class CaseRunner(CaseRunnerBase):
         args1 = self.genr8_dex_args_post(typ)
         # Get class
         cls = self._dex_cls[typ]
-        # Check for negative sign
-        c = -1.0 if compid.startswith('-') else 1.0
         # Use custom clas to instantiate
         db = cls(*args0, compid, *args1)
-        # Scale
-        if c != 1.0:
-            db *= c
         # Output
         return db
 
@@ -2557,6 +2560,32 @@ class CaseRunner(CaseRunnerBase):
         # Output
         return args
 
+    # Apply transformations
+    def transform_dex(self, comp: str, db: DataKit):
+        r"""Apply transformations for a data extraction component
+
+        :Call:
+            >>> runner.transform_dex(comp, db)
+        :Inputs:
+            *runner*: :class:`CaseRunner`
+                Controller to run one case of solver
+            *comp*: :class:`str`
+                Name of DataBook component
+            *db*: :class:`cape.dkit.rdb.DataKit`
+                Untransformed data extraction
+        :Versions:
+            * 2025-08-05 ``@ddalle``: v1.0
+        """
+        # Get component type
+        typ = self.get_dex_type(comp)
+        # Get name of transformation function
+        name = f"transform_dex_{typ}"
+        # Get function if any
+        fn = getattr(self, name, None)
+        # Call it if appropriate
+        if callable(fn):
+            fn(db)
+
    # --- Force & Moment ---
     def transform_dex_fm(self, comp: str, fm: CaseFM):
         r"""Apply transformations for a force & moment data extraction
@@ -2584,6 +2613,7 @@ class CaseRunner(CaseRunnerBase):
         # Loop through transformations
         for topts in transforms:
             fm.TransformFM(topts, cntl.x, i)
+
    # --- Data manipulation ---
 
    # --- Status ---
