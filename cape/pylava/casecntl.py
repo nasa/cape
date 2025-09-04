df4c20a2ab305e9fb66be7c233320fdba6530f8b
Derek J. Dalle <derek.j.dalle@nasa.gov>
2025-09-04 13:04:30 -0700

    Fix bug w/ LAVA-Cart regarding restart
    Section must be entirely gone if not restarting

--- cape/pylava/casecntl.py
+++ cape/pylava/casecntl.py
@@ -408,7 +408,7 @@ class CaseRunner(casecntl.CaseRunner):
         # Remove it if not a restart
         if restartfile is None:
             # Remove restart file if previously set
-            opts["solver defaults"]["restart"].pop("file")
+            opts["solver defaults"].pop("restart", None)
         # Write
         opts.write()
 
