ca7367338b702082912e1bb6e54ce2d36f8445b4
Derek J. Dalle <derek.j.dalle@nasa.gov>
2026-02-23 13:14:15 -0800

    Change x.save -> grid.in when in pyover lineload/ folder

--- cape/pyover/casecntl.py
+++ cape/pyover/casecntl.py
@@ -703,7 +703,7 @@ class CaseRunner(casecntl.CaseRunner):
 
     def _run_splitmx(self, src: MeshFileMeta):
         # Check for existing ``q.save``
-        if os.path.isfile("q.save") and os.path.isfile("x.save"):
+        if os.path.isfile("q.save") and os.path.isfile("grid.in"):
             if checkqt("q.save") >= checkqt(os.path.join('..', src.q)):
                 # Already up-to-date
                 self.log_verbose(f"{src.q} -> q.save up-to-date", parent=1)
@@ -711,7 +711,7 @@ class CaseRunner(casecntl.CaseRunner):
         # Read run matrix
         cntl = self.read_cntl()
         # Get splitmq/splitmx options
-        fsplitmx = cntl.opts.get_ConfigSplitmx()
+        fsplitmx = cntl.opts.get_DataBook_splitmx()
         # Check for splitmq
         if fsplitmx:
             # Absolutize
@@ -723,11 +723,11 @@ class CaseRunner(casecntl.CaseRunner):
                 # Link ``q`` file
                 self.link_file(os.path.join("..", src.x), "x.vol")
                 # Edit splitmq.i
-                EditSplitmqI(abssplitmx, "splitmx.i", "x.vol", "x.save")
+                EditSplitmqI(abssplitmx, "splitmx.i", "x.vol", "grid.in")
                 # Run it
                 self.splitmx("splitmx.i")
         # Otherwise use the volume file
-        self.link_file(os.path.join("..", src.x), "x.save")
+        self.link_file(os.path.join("..", src.x), "grid.in")
 
     def splitmq(self, fsplitmq: str = "splitmq.i"):
         r"""Run ``splitmq`` to extract surface and second-layer sol data
