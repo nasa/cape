69f942d4ed5cc2f9f870bfa0dcda6a250af37d7f
Derek J. Dalle <derek.j.dalle@nasa.gov>
2026-05-27 14:09:51 -0700

    Write initial part of test for pycart-intersect

--- test/901_pycart/003_isect/test_intersect_c3d.py
+++ test/901_pycart/003_isect/test_intersect_c3d.py
@@ -1,12 +1,10 @@
 
 # Standard library
-import sys
 
 # Third-party imports
 import testutils
 
 # Local imports
-from cape.pycart import cli
 from cape.pycart.cntl import Cntl
 
 
@@ -14,22 +12,9 @@ from cape.pycart.cntl import Cntl
 TEST_FILES = (
     "pyCart.json",
     "matrix.csv",
-    "Config.xml",
+    "bullet.json",
     "bullet.tri",
-    "test.[0-9][0-9].out"
 )
-
-
-# Test 'cape -c'
-@testutils.run_sandbox(__file__, TEST_FILES)
-def test_01_c():
-    # Split command and add `-m` prefix
-    cmdlist = [sys.executable, "-m", "cape.pycart", "-c"]
-    # Run the command
-    stdout, _, _ = testutils.call_o(cmdlist)
-    # Check outout
-    result = testutils.compare_files(stdout, "test.01.out")
-    assert result.line1 == result.line2
 
 
 # Run a case
@@ -39,16 +24,3 @@ def test_02_run():
     cntl = Cntl()
     # Run first case
     cntl.SubmitJobs(I="0")
-    # Collect aero
-    cli.main(["pycart", "-fm", "-I", "0"])
-    # Read databook
-    db = cntl.read_dex("bullet_no_base")
-    # Get value
-    CA = db["CA"][0]
-    # Test value
-    assert abs(CA - 0.745) <= 0.0025
-
-
-if __name__ == "__main__":
-    test_01_c()
-    test_02_run()
