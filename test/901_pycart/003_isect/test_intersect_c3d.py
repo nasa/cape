60b7c4afe347889d6cd141a8b6dcf3b7aacb41d1
Derek J. Dalle <derek.j.dalle@nasa.gov>
2026-05-27 14:27:18 -0700

    Update pycart settings for intersect test

--- test/901_pycart/003_isect/test_intersect_c3d.py
+++ test/901_pycart/003_isect/test_intersect_c3d.py
@@ -18,7 +18,7 @@ TEST_FILES = (
 
 
 # Run a case
-@testutils.run_sandbox(__file__, fresh=False)
+@testutils.run_sandbox(__file__, TEST_FILES)
 def test_02_run():
     # Instantiate
     cntl = Cntl()
