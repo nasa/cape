b327b7c73eef06d5b984e09acc73155387e6e7cd
Derek J. Dalle <derek.j.dalle@nasa.gov>
2026-05-28 01:05:16 -0700

    Finish pycart-intersect test

--- test/901_pycart/003_isect/test_intersect_c3d.py
+++ test/901_pycart/003_isect/test_intersect_c3d.py
@@ -1,5 +1,6 @@
 
 # Standard library
+import os
 
 # Third-party imports
 import testutils
@@ -13,6 +14,7 @@ TEST_FILES = (
     "pyCart.json",
     "matrix.csv",
     "bullet.json",
+    "bullet.xml",
     "bullet.tri",
 )
 
@@ -24,3 +26,14 @@ def test_02_run():
     cntl = Cntl()
     # Run first case
     cntl.SubmitJobs(I="0")
+    # Get runner for that case
+    runner = cntl.ReadCaseRunner(0)
+    # Enter folder
+    os.chdir(runner.root_dir)
+    # Check for files
+    assert os.path.isfile("Components.tri")
+    assert os.path.isfile("Components.c.tri")
+    assert os.path.isfile("Components.i.tri")
+    # Check that Cart3D ran
+    assert runner.get_iter() > 1
+
